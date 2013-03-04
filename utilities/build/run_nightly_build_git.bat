fe09b49852df378235bcf9bedfe7164e65a147e9
keithbrown <keith_brown@mentor.com>
2013-03-04 10:40:55 -0500

    This batch file is meant to be called by a windows scheduled task on a nightly basis.  It handles the sequencing of the scripts in the build chain and makes the transition from DOS to bash scripting under cygwin.  For #1.

---
+++ utilities/build/run_nightly_build_git.bat
@@ -0,0 +1,29 @@
+rem
+rem  This script runs the nightly build.
+rem 
+
+@echo off
+
+set BUILD_ROOT=C:\builds
+set BRANCH=master
+set GIT_REPO_ROOT="/git/xtuml"
+echo set BUILD_ROOT=%BUILD_ROOT%
+echo set BRANCH=%BRANCH%
+echo set GIT_REPO_ROOT=%GIT_REPO_ROOT%
+
+C:
+chdir %BUILD_ROOT%
+
+path c:\cygwin\bin;%Path%
+
+unix2dos init_git_repositories.sh
+bash init_git_repositories.sh "%BRANCH%" "%GIT_REPO_ROOT%" "yes"
+
+cp -fv %GIT_REPO_ROOT%/internal/utilities/build/configure_build_process.sh .
+unix2dos configure_build_process.sh
+bash configure_build_process.sh %BRANCH% %GIT_REPO_ROOT% nonrelease 1>2> cfg_output.log
+
+rem bash create_bp_release.sh %BRANCH% %GIT_REPO_ROOT% 1>2> nb_output.log
+rem change to post_process_build bash create_nightly_build.sh %BRANCH% nonrelease 1>2> nb_output.log
+
+echo end of run_nightly_build_git.bat
