8d4c595ba2100041a024f295e71ab0d105f4f5c9
Keith Brown <keith_brown@mentor.com>
2013-07-09 16:45:27 -0400

    Update configure_build_process.sh
    job: #1
    Move log files to new location.

--- utilities/build/configure_build_process.sh
+++ utilities/build/configure_build_process.sh
@@ -147,6 +147,10 @@ if [ ! -x $log_dir ]; then
     mkdir $log_dir
 fi
 
+# Move the log files created by earlier scripts into the new log dir
+mv ${base_dir}/cfg_output.log ${log_dir}
+mv ${base_dir}/diff.log ${log_dir}
+
 cd ${build_dir}
 configure_build_files
 configure_installer_files
