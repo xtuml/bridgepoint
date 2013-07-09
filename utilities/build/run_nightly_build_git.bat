784103fbd07725a52ecab6843cba8030e386bce1
Keith Brown <keith_brown@mentor.com>
2013-07-09 18:37:07 -0400

    Update run_nightly_build_git.bat
    job: #1 - add in tool to get data from SVN.  Output more status info.

--- utilities/build/run_nightly_build_git.bat
+++ utilities/build/run_nightly_build_git.bat
@@ -1,3 +1,5 @@
+@echo off
+
 rem
 rem  This script runs the nightly build.
 rem
@@ -5,15 +7,12 @@ rem  Since it is the starting point for the build chain, it must be manually put
 rem  place for the build server to run.
 rem 
 
-@echo off
 
 set BUILD_ROOT=C:\builds
 set BRANCH=master
-set SVN_BRANCH=trunk
 set GIT_REPO_ROOT="/git/xtuml"
 echo set BUILD_ROOT=%BUILD_ROOT%
 echo set BRANCH=%BRANCH%
-echo set SVN_BRANCH=%SVN_BRANCH%
 echo set GIT_REPO_ROOT=%GIT_REPO_ROOT%
 
 C:
@@ -21,17 +20,24 @@ chdir %BUILD_ROOT%
 
 path c:\cygwin\bin;%Path%
 
-unix2dos init_git_repositories.sh
+dos2unix init_git_repositories.sh
+echo Initializing git repositories.
 bash init_git_repositories.sh "%BRANCH%" "%GIT_REPO_ROOT%" "yes"
+echo Done. 
 
-unix2dos configure_eclipse_base.sh
-bash configure_eclipse_base.sh "%BRANCH%"
+cp -fv %GIT_REPO_ROOT%/internal/utilities/build/configure_eclipse_bases.sh .
+dos2unix configure_eclipse_bases.sh
+echo Configuring eclipse bases.
+bash configure_eclipse_bases.sh "%BRANCH%" "nonrelease" 1>2> cfg_output.log
+echo Done. 
 
 cp -fv %GIT_REPO_ROOT%/internal/utilities/build/configure_build_process.sh .
-unix2dos configure_build_process.sh
+dos2unix configure_build_process.sh
+echo Configuring build process.
 bash configure_build_process.sh %BRANCH% %GIT_REPO_ROOT% nonrelease 1>2> cfg_output.log
+echo Done. 
 
-bash create_bp_release.sh %BRANCH% %GIT_REPO_ROOT% 1>2> nb_output.log
+rem bash create_bp_release.sh %BRANCH% %GIT_REPO_ROOT% 1>2> nb_output.log
 rem change to post_process_build bash create_nightly_build.sh %BRANCH% nonrelease 1>2> nb_output.log
 
 echo end of run_nightly_build_git.bat
