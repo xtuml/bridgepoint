7955cb122177d9ad7c58f8ed116b9e32e7d218e6
Keith Brown <keith_brown@mentor.com>
2013-07-09 15:08:13 -0400

    Update run_nightly_build_git.bat
    Job: #1
    Adding note about manual placement and hooking in SVN export script.

--- utilities/build/run_nightly_build_git.bat
+++ utilities/build/run_nightly_build_git.bat
@@ -1,14 +1,19 @@
 rem
 rem  This script runs the nightly build.
+rem
+rem  Since it is the starting point for the build chain, it must be manually put into 
+rem  place for the build server to run.
 rem 
 
 @echo off
 
 set BUILD_ROOT=C:\builds
 set BRANCH=master
+set SVN_BRANCH=trunk
 set GIT_REPO_ROOT="/git/xtuml"
 echo set BUILD_ROOT=%BUILD_ROOT%
 echo set BRANCH=%BRANCH%
+echo set SVN_BRANCH=%SVN_BRANCH%
 echo set GIT_REPO_ROOT=%GIT_REPO_ROOT%
 
 C:
@@ -18,6 +23,9 @@ path c:\cygwin\bin;%Path%
 
 unix2dos init_git_repositories.sh
 bash init_git_repositories.sh "%BRANCH%" "%GIT_REPO_ROOT%" "yes"
+
+unix2dos configure_eclipse_base.sh
+bash configure_eclipse_base.sh "%BRANCH%"
 
 cp -fv %GIT_REPO_ROOT%/internal/utilities/build/configure_build_process.sh .
 unix2dos configure_build_process.sh
