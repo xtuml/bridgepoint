78a8f4296c1a7cc6bd1aeccf5cfb5c167424381f
Travis London <travis.london@gmail.com>
2013-10-08 13:19:51 -0700

    Job: #50 Add header

--- src/com.mentor.nucleus.bp.model.compare.test/src/com/mentor/nucleus/bp/model/compare/test/ModelMergeTests.java
+++ src/com.mentor.nucleus.bp.model.compare.test/src/com/mentor/nucleus/bp/model/compare/test/ModelMergeTests.java
@@ -1,3 +1,13 @@
+//========================================================================
+//
+//File:      ModelMergeTests.java
+//
+//Copyright 2005-2013 Mentor Graphics Corporation. All rights reserved.
+//
+//========================================================================
+//This document contains information proprietary and confidential to
+//Mentor Graphics Corp., and is not for external distribution.
+//========================================================================
 package com.mentor.nucleus.bp.model.compare.test;
 
 import java.util.List;
