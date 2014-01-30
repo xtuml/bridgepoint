0910d0413f0e798126d828fec92985b717ec4fdd
cmccausl <cmccausl@cmccaus-vm-w7-1.mgc.mentorg.com>
2014-01-30 04:36:00 -0800

    Job #146 Create and use alternate instance list implementation for runtime classes.

---
+++ src/com.mentor.nucleus.bp.core/src/com/mentor/nucleus/bp/core/common/RuntimeInstanceList.java
@@ -0,0 +1,54 @@
+
+package com.mentor.nucleus.bp.core.common;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+//========================================================================
+//
+//File:      $RCSfile: InstanceList.java,v $
+//Version:   $Revision: 1.34 $
+//Modified:  $Date: 2013/05/10 13:26:32 $
+//
+//(c) Copyright 2005-2013 by Mentor Graphics Corp. All rights reserved.
+//
+//========================================================================
+//This document contains information proprietary and confidential to
+//Mentor Graphics Corp., and is not for external distribution.
+//======================================================================== 
+
+@SuppressWarnings("serial")
+public class RuntimeInstanceList extends InstanceList {
+
+	public RuntimeInstanceList(ModelRoot aRoot, Class aType) {
+		super(aRoot, aType);
+	}
+
+	private RuntimeInstanceList(ArrayList clone,
+			HashMap<BPElementID, NonRootModelElement> instanceMap,
+			ModelRoot root, Class type) {
+		super(clone, instanceMap, root, type);
+	}
+
+	@Override
+	Object clone(ArrayList clone,
+			HashMap<BPElementID, NonRootModelElement> instanceMap,
+			ModelRoot root, Class type) {
+		  return new RuntimeInstanceList(clone, instanceMap, root, type );
+	}
+	
+	public synchronized boolean contains(NonRootModelElement element) {
+		return super.lastIndexOf(element) != -1;
+	}
+
+	public synchronized boolean remove(Object key, Object object) {
+		boolean removed = super.remove(super.lastIndexOf(object)) != null;
+		if (removed) {
+			if (size() == 0) {
+				instanceMap.clear();
+			}
+			removeOldKey(key, object);
+		}
+		return removed;
+	}
+
+}
