3b805e2f24f8fdead6bcebe8c513b9686065d48f
rmulvey <robert_mulvey@mentor.com>
2014-02-11 16:37:42 -0500

    #133 #170 #160 Introduced test results.

--- src/com.mentor.nucleus.bp.ui.canvas.test/src/com/mentor/nucleus/bp/ui/canvas/test/CanvasStateMachineCopyPasteTests.java
+++ src/com.mentor.nucleus.bp.ui.canvas.test/src/com/mentor/nucleus/bp/ui/canvas/test/CanvasStateMachineCopyPasteTests.java
@@ -1101,6 +1101,10 @@ public class CanvasStateMachineCopyPasteTests extends CanvasTest {
 		UITestingUtilities.pasteClipboardContents(new Point(200, 200),
 				destinationEditor);
 		BaseTest.dispatchEvents(0);
+boolean debug = false;
+while (debug) {
+	PlatformUI.getWorkbench().getDisplay().readAndDispatch();
+}
 		validateOrGenerateResults(destinationEditor, generateResults);
 	}
 	
