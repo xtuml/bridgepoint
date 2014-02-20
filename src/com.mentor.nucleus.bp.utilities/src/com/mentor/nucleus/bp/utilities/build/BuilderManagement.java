620f1949e6e1f7aa454be9c9a3dc1b28733a1d30
keithbrown <keith_brown@mentor.com>
2014-02-20 18:04:41 -0500

    job #177 - add utility function to find a builder based on a regex.

--- src/com.mentor.nucleus.bp.utilities/src/com/mentor/nucleus/bp/utilities/build/BuilderManagement.java
+++ src/com.mentor.nucleus.bp.utilities/src/com/mentor/nucleus/bp/utilities/build/BuilderManagement.java
@@ -106,6 +106,39 @@ public class BuilderManagement {
                     + " project.", e);
         }
     }
+    
+    public static int findBuilder(IProject project, final String builderNameRegex) {
+        int position = -1;
+        
+        try {
+            if (project.isOpen()) {
+                // Get project description and then the associated build commands
+                IProjectDescription desc = project.getDescription();
+                ICommand[] commands = desc.getBuildSpec();
+
+                for (int i = 0; i < commands.length; ++i) {
+                    // Check for builder in enabled state
+                    if (commands[i].getBuilderName().matches(builderNameRegex)) {
+                        position = i;
+                        break;
+                    }
+                    // Check for builder in disabled state
+                    Map<?, ?> args = commands[i].getArguments();
+                    if (args != null) {
+                        String value = (String) args.get("LaunchConfigHandle");
+                        if (value != null && value.matches(builderNameRegex)) {
+                            position = i;
+                            break;
+                        }
+                    }
+                }
+            }
+        } catch (CoreException e) {
+            CorePlugin.logError("Error running findBuilder for "
+                    + " project.", e);
+        }
+        return position;
+    }
 
     public static void findAndRemoveBuilder(IProject project, final String builderNameRegex) {
         try {
