5a440b7710b97a2597c226207688b10eae4a8099
keithbrown <keith_brown@mentor.com>
2014-02-19 23:45:16 -0500

    job #177 - committing work in progress to remove old builder info

--- src/com.mentor.nucleus.bp.mc/src/com/mentor/nucleus/bp/mc/tools/SwitchProjectModelCompilerWizard.java
+++ src/com.mentor.nucleus.bp.mc/src/com/mentor/nucleus/bp/mc/tools/SwitchProjectModelCompilerWizard.java
@@ -16,38 +16,15 @@ import java.lang.reflect.InvocationTargetException;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectDescription;
-import org.eclipse.core.resources.IWorkspace;
-import org.eclipse.core.resources.IWorkspaceRunnable;
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.resources.WorkspaceJob;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.core.runtime.Platform;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.operation.IRunnableWithProgress;
-import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.IWizardContainer;
-import org.eclipse.ui.IWorkbench;
-import org.eclipse.ui.PlatformUI;
 
 import com.mentor.nucleus.bp.core.CorePlugin;
-import com.mentor.nucleus.bp.core.Modeleventnotification_c;
-import com.mentor.nucleus.bp.core.Ooaofooa;
-import com.mentor.nucleus.bp.core.SystemModel_c;
-import com.mentor.nucleus.bp.core.XtUMLNature;
-import com.mentor.nucleus.bp.core.common.BaseModelDelta;
-import com.mentor.nucleus.bp.core.common.ClassQueryInterface_c;
-import com.mentor.nucleus.bp.core.common.ComponentResourceListener;
-import com.mentor.nucleus.bp.core.common.ModelRoot;
-import com.mentor.nucleus.bp.core.common.PersistableModelComponent;
-import com.mentor.nucleus.bp.core.common.PersistenceManager;
 import com.mentor.nucleus.bp.core.ui.DelegatingWizard;
 import com.mentor.nucleus.bp.core.ui.WizardDelegateChooserPage;
-import com.mentor.nucleus.bp.core.ui.WizardNewSystemCreationPage;
 
 
 public class SwitchProjectModelCompilerWizard extends DelegatingWizard {
@@ -59,14 +36,7 @@ public class SwitchProjectModelCompilerWizard extends DelegatingWizard {
     private IProject currentProject;
     
     SwitchProjectModelCompilerWizard(IProject project) {
-        currentProject = project;
-    }
-    
-    /* (non-Javadoc)
-     * @see org.eclipse.ui.IWorkbenchWizard#init(org.eclipse.ui.IWorkbench, org.eclipse.jface.viewers.IStructuredSelection)
-     */
-    public void init(IWorkbench workbench, IStructuredSelection selection) {
-        super.init(workbench, selection);
+        currentProject = project; // TODO - needed?
         setNeedsProgressMonitor(true);
         String[] mcis = null;
         if (getDelegatingWizard() != null) {
@@ -75,13 +45,14 @@ public class SwitchProjectModelCompilerWizard extends DelegatingWizard {
                 addPage(new WizardDelegateChooserPage(
                         "switchxtUMLModelCompilerChooser",
                         "Multiple xtUML Model Compilers found",
-                        "Select the model compiler to use with this (" + currentProject.getName() + ") xtUML project",
+                        "Select the model compiler to use with this (" + project.getName() + ") xtUML project",
                         "Available xtUML model compilers:"));
             } else if (mcis.length == 1) {
                 setDelegate(mcis[0]);
             }
         }
     }
+    
     public String getExtensionPoint() {
         return MC_EXTENSION_POINT_NAME;
     }
@@ -108,7 +79,6 @@ public class SwitchProjectModelCompilerWizard extends DelegatingWizard {
                 //Unit test invoke it without setting container/progress monitor.
                 return performMCSwitch(new NullProgressMonitor());
             }
-
         } catch (InvocationTargetException e) {
             CorePlugin.logError("Internal error: " + e.getMessage(), e); //$NON-NLS-1$
             return false;
@@ -120,52 +90,15 @@ public class SwitchProjectModelCompilerWizard extends DelegatingWizard {
         return op.result;
     }
     
-    class ProjectMCSwitchRunnable extends WorkspaceJob {
-        public ProjectMCSwitchRunnable(String name) {
-            super(name);
-        }
-
-        boolean returnVal = false;
-
-        @Override
-        public IStatus runInWorkspace(IProgressMonitor monitor)
-                throws CoreException {
-            // we must do this on the UI Thread
-            PlatformUI.getWorkbench().getDisplay().syncExec(new Runnable() {
-                
-                @Override
-                public void run() {
-                    // TODO SKB - the meat goes here
-                    /*IProject newProject = createProject();
-                    if (newProject != null) {
-                        XtUMLNature.addNature(newProject);
-                        final String name = m_creationPage.getProjectName();
-                        SwitchProjectModelCompilerWizard.createSystemModel(newProject, name);
-                        returnVal = SwitchProjectModelCompilerWizard.super.performFinish(newProject);
-                    } */
-                    returnVal = true;
-                }
-            });
-            if(!returnVal) {
-                return Status.CANCEL_STATUS;
-            }
-            return Status.OK_STATUS;
-        }
-    }
-    
     /* (non-Javadoc)
      * @see org.eclipse.jface.wizard.IWizard#performFinish()
      */
     public boolean performMCSwitch(IProgressMonitor monitor) {
-        ProjectMCSwitchRunnable runnable = new ProjectMCSwitchRunnable("xtUML Project MC Switch Job");
-        runnable.setPriority(Job.INTERACTIVE);
-        runnable.schedule();
-        try {
-            runnable.join();
-        } catch (InterruptedException e) {
-            CorePlugin.logError("Unable to switch xtUML project model compiler", e);
-        }
-        return runnable.returnVal;
+        boolean rval = true;
+        
+        rval = super.performFinish(currentProject);
+        
+        return rval;
     }
 
 }
