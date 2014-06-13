f1a4d657fb6006c79e752be18d7acc854efc2509
rmulvey <robert_mulvey@mentor.com>
2014-06-13 11:48:50 -0400

    #236 Added support for the remaining required unit tests, including support for testing of Compare With...   The compare is support via the synchronize view, so I added support for the synchronize view too.

--- src/com.mentor.nucleus.bp.model.compare.test/src/com/mentor/nucleus/bp/model/compare/test/ModelMergeTests2.java
+++ src/com.mentor.nucleus.bp.model.compare.test/src/com/mentor/nucleus/bp/model/compare/test/ModelMergeTests2.java
@@ -2,6 +2,10 @@ package com.mentor.nucleus.bp.model.compare.test;
 
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.TreeItem;
+import org.eclipse.ui.IViewPart;
 import org.eclipse.ui.PlatformUI;
 
 import com.mentor.nucleus.bp.core.CorePlugin;
@@ -14,6 +18,7 @@ import com.mentor.nucleus.bp.core.common.ClassQueryInterface_c;
 import com.mentor.nucleus.bp.test.common.BaseTest;
 import com.mentor.nucleus.bp.test.common.CompareTestUtilities;
 import com.mentor.nucleus.bp.test.common.GitUtil;
+import com.mentor.nucleus.bp.test.common.UITestingUtilities;
 import com.mentor.nucleus.bp.test.common.ZipUtil;
 import com.mentor.nucleus.bp.ui.explorer.ModelContentProvider;
 import com.mentor.nucleus.bp.ui.explorer.ModelLabelProvider;
@@ -140,10 +145,106 @@ public class ModelMergeTests2  extends BaseTest {
 				"m_a1", "m_a2", "s_a1", "s_a2", "m_o1", "m_o2", "s_o1", "s_o2",
 				"Class State Machine" };
 		verifyOrder(orderedElements, clazz);
+	}
+
+	public void testMergeClassSortedElementsWithMiddleAddition() throws Exception {
+		// Merge Library::Tracking::WorkoutTimer (1 incoming change in master, 1 outgoing change from slave)
+		String projectName = "GPS Watch";
+		// import git repository from models repo
+		GitUtil.loadRepository(test_repositories
+				+ "/" + projectName, "slave3");
+		// import test project
+		GitUtil.loadProject(projectName, projectName, "slave3");
+		// merge the test branch
+		GitUtil.mergeBranch("master3", projectName, "slave3");
+		// start the merge tool
+		GitUtil.startMergeTool(projectName);
+
+		CompareTestUtilities.copyAllNonConflictingChangesFromRightToLeft();
+		// validate
+		assertTrue("Found conflicting changes remaining.", CompareTestUtilities
+				.getConflictingChanges().size() == 0);
+		assertTrue("Found incoming changes remaining.", CompareTestUtilities
+				.getIncomingChanges().size() == 0);
 		
-		// TODO: Merge Library::Tracking::WorkoutTimer (1 incoming change in master, 1 outgoing change from slave)
-		// TODO: Merge test1::iface1 (1 incoming change in master, 1 outgoing change from slave)
-		// TODO: CompareWith... (just run compare with and open the editor for one of he changes, 
+		CompareTestUtilities.flushMergeEditor();
+
+		m_sys = getSystemModel(projectName);		
+		ModelClass_c clazz = ModelClass_c.getOneO_OBJOnR8001(PackageableElement_c
+				.getManyPE_PEsOnR8000(Package_c.getManyEP_PKGsOnR1401(m_sys)),
+				new ClassQueryInterface_c() {
+
+					@Override
+					public boolean evaluate(Object candidate) {
+						return ((ModelClass_c) candidate).getName().equals(
+								"WorkoutTimer");
+					}
+				});
+		
+		String[] orderedElements = new String[] { "current_state", "time",
+				"timer", "activate", "m_middle", "desctivate", 
+				"Instance State Machine" };
+		verifyOrder(orderedElements, clazz);
+	}
+	
+	
+	public void testMergeInterfaceSortedElementsOneAddition() throws Exception {
+		// Merge test1::iface1 (1 incoming change in master, 1 outgoing change from slave)
+		String projectName = "GPS Watch";
+		// import git repository from models repo
+		GitUtil.loadRepository(test_repositories
+				+ "/" + projectName, "slave4");
+		// import test project
+		GitUtil.loadProject(projectName, projectName, "slave4");
+		// merge the test branch
+		GitUtil.mergeBranch("master4", projectName, "slave4");
+		// start the merge tool
+		GitUtil.startMergeTool(projectName);
+
+		CompareTestUtilities.copyAllNonConflictingChangesFromRightToLeft();
+		// validate
+		assertTrue("Found conflicting changes remaining.", CompareTestUtilities
+				.getConflictingChanges().size() == 0);
+		assertTrue("Found incoming changes remaining.", CompareTestUtilities
+				.getIncomingChanges().size() == 0);
+		
+		CompareTestUtilities.flushMergeEditor();
+
+		m_sys = getSystemModel(projectName);		
+		Interface_c iface = Interface_c.getOneC_IOnR8001(PackageableElement_c
+				.getManyPE_PEsOnR8000(Package_c.getManyEP_PKGsOnR1401(m_sys)),
+				new ClassQueryInterface_c() {
+
+					@Override
+					public boolean evaluate(Object candidate) {
+						return ((Interface_c) candidate).getName().equals(
+								"iface1");
+					}
+				});
+		
+		String[] orderedElements = new String[] { "s_op1", "m_op1"};
+		verifyOrder(orderedElements, iface);
+		
+		// There should be no error log entries (shutdown will verify this)
+	}
+	
+	public void testCompareWith() throws Exception {
+		String projectName = "GPS Watch";
+		// import git repository from models repo
+		GitUtil.loadRepository(test_repositories
+				+ "/" + projectName, "slave1");
+		// import test project
+		GitUtil.loadProject(projectName, projectName, "slave1");
+		
+		GitUtil.compareWithBranch("master1", projectName, "slave1");
+
+		String[] actualResult = UITestingUtilities.getCompareViewStructuralDifferences("Pkg1");
+		String[] expectedResult = new String[] { "m_op1", "m_op2", "sameName",
+				"s_op1", "s_op2", "sameName", "m_sig1", "m_sig2", "sameName",
+				"s_sig1", "s_sig2" };
+		
+		assertTrue("The comparision contains all expected elements", actualResult.equals(expectedResult));
+		
 		// There should be no error log entries (shutdown will verify this)
 	}
 	
