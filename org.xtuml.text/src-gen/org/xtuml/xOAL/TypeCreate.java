80c733150a552a7fd014dfcb4474c7a748744eaf
travis.london@gmail.com <travis.london@gmail.com>
2014-12-18 05:55:09 -0700

    Job: #506 Add new xtext plug-ins with hook to custom editor

---
+++ org.xtuml.text/src-gen/org/xtuml/xOAL/TypeCreate.java
@@ -0,0 +1,22 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+
+ */
+package org.xtuml.xOAL;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Type Create</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see org.xtuml.xOAL.XOALPackage#getTypeCreate()
+ * @model
+ * @generated
+ */
+public interface TypeCreate extends TypeObjectStatement
+{
+} // TypeCreate
