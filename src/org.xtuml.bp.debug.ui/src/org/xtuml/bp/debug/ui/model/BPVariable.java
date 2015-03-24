e07378bae80522ed27a152da6eeb88b6666ed56d
HebaKhaled <HebaKhaled08@gmail.com>
2015-03-24 23:17:53 +0200

    job:7540
    Add more information for Instance variables in Variable view such as
    related instances update work

--- src/org.xtuml.bp.debug.ui/src/org/xtuml/bp/debug/ui/model/BPVariable.java
+++ src/org.xtuml.bp.debug.ui/src/org/xtuml/bp/debug/ui/model/BPVariable.java
@@ -1,27 +1,41 @@
 package org.xtuml.bp.debug.ui.model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import org.eclipse.debug.core.DebugException;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.model.IDebugTarget;
 import org.eclipse.debug.core.model.IValue;
 import org.eclipse.debug.core.model.IVariable;
 
+import org.xtuml.bp.core.Association_c;
 import org.xtuml.bp.core.AttributeValue_c;
 import org.xtuml.bp.core.Attribute_c;
 import org.xtuml.bp.core.BridgeParameter_c;
+import org.xtuml.bp.core.ClassAsAssociatedOneSide_c;
+import org.xtuml.bp.core.ClassAsAssociatedOtherSide_c;
+import org.xtuml.bp.core.CorePlugin;
 import org.xtuml.bp.core.DataItemValue_c;
 import org.xtuml.bp.core.FunctionParameter_c;
 import org.xtuml.bp.core.InstanceHandle_c;
 import org.xtuml.bp.core.InstanceSet_c;
 import org.xtuml.bp.core.Instance_c;
+import org.xtuml.bp.core.LinkParticipation_c;
+import org.xtuml.bp.core.Link_c;
+import org.xtuml.bp.core.LinkedAssociation_c;
 import org.xtuml.bp.core.LocalReference_c;
 import org.xtuml.bp.core.LocalValue_c;
 import org.xtuml.bp.core.Local_c;
 import org.xtuml.bp.core.OperationParameter_c;
+import org.xtuml.bp.core.PendingEvent_c;
 import org.xtuml.bp.core.PropertyParameter_c;
 import org.xtuml.bp.core.RuntimeValue_c;
 import org.xtuml.bp.core.StateMachineEventDataItem_c;
+import org.xtuml.bp.core.StateMachineEvent_c;
+import org.xtuml.bp.core.StateMachineState_c;
 import org.xtuml.bp.core.TransientVar_c;
+import org.xtuml.bp.core.Transition_c;
 import org.xtuml.bp.core.ValueInArray_c;
 import org.xtuml.bp.core.ValueInStructure_c;
 import org.xtuml.bp.core.Variable_c;
@@ -29,16 +43,32 @@ import org.xtuml.bp.core.Variable_c;
 public class BPVariable extends BPDebugElement implements IVariable {
 	
 	Object value = null;
+	Object[] linkedValues  = null;
+	Object instance = null;
 
-	public BPVariable(IDebugTarget debugTarget, ILaunch launch, Object variable) {
+	public BPVariable(IDebugTarget debugTarget, ILaunch launch, Object variable, String Name) {
 		super((BPDebugTarget)debugTarget, launch);
 		value = variable;
+		name = Name;
+		linkedValues = null;
+		instance = null;
+	}
+	public BPVariable(IDebugTarget debugTarget, ILaunch launch, Object variable, String Name, Object[] LinkedValues, Object Instance) {
+		super((BPDebugTarget)debugTarget, launch);
+		value = variable;
+		name = Name;
+		linkedValues = LinkedValues;
+		instance = Instance;
 	}
 
 	public IValue getValue() throws DebugException {
 		if (value != null)
-		  return new BPValue(getDebugTarget(), getLaunch(), value);
+		  return new BPValue(getDebugTarget(), getLaunch(), value, name, this);
 		return null;
+	}
+	
+	public void setName(String Name){
+		super.setName(Name);
 	}
 
 	public String getReferenceTypeName() throws DebugException {
@@ -106,7 +136,7 @@ public class BPVariable extends BPDebugElement implements IVariable {
     	if(param != null) {
     		return param.getName();
     	}
-      }
+      } 
       else if (value instanceof AttributeValue_c) {
       	Attribute_c attr = Attribute_c.getOneO_ATTROnR2910((AttributeValue_c)value);
       	if (attr != null) {
@@ -140,8 +170,84 @@ public class BPVariable extends BPDebugElement implements IVariable {
       else if (value instanceof Instance_c){
     	  return "instance";
       }
+      
+      try{
+    	  if (value instanceof StateMachineState_c){
+    		  return "Current State";
+    	  }
+    	  if (value instanceof StateMachineEvent_c){
+    		  return "Last Executed Transition";
+    	  }
+    	  if (value instanceof Association_c){ 
+    		  if (name == "Origin Of") {
+    			  Link_c[] instanceLinks = getInstanceLinksForAnAssociation();
+    			  LinkParticipation_c lp = LinkParticipation_c .getOneI_LIPOnR2902( instanceLinks);
+    			  if (lp == null)
+    				  return LinkParticipation_c.getOneI_LIPOnR2903( instanceLinks).getLabel();
+    			  else 
+    				  return lp.getLabel();
+
+    		  } else if (name == "Destination Of") {
+    			  Link_c[] instanceLinks = getInstanceLinksForAnAssociation();
+    			  LinkParticipation_c lp = LinkParticipation_c .getOneI_LIPOnR2901( instanceLinks);
+    			  if (lp == null)
+    				  return LinkParticipation_c .getOneI_LIPOnR2903( instanceLinks).getLabel();
+    			  else 
+    				  return lp.getLabel();
+    		  } 
+    		  else if (name == "Associator For") {
+    			  return "R" + ((Association_c)value).getNumb();
+
+    		  }
+    		  return "R" + ((Association_c)value).getNumb();
+    	  }
+    	  else if ( value instanceof PendingEvent_c[]){
+    		  return "Pending Events";
+    	  }
+    	  else if ( value instanceof PendingEvent_c){
+    		  return "Event";
+    	  }
+      }catch (Exception e) {
+    	  return "Error: Variable for local value not found.";
+      }
+      
       return "Error: Variable for local value not found.";
     }
+    
+    /**
+	 *  Get association links only selected association and instance.
+	 *  
+	 *  the linkedValues contains a list of all selected instance in particular
+	 *  linking direction.
+	 *  While querying for Association links will return an array for selected
+	 *  instance links and other instances links
+	 *  This method return the common links in linkedValues and Association
+	 *  links array.
+	 * 
+	 * @return an Link_c array for selected instance in selected association.
+	 */
+    public Link_c[] getInstanceLinksForAnAssociation() {
+		Link_c[] allOriginlinks = (Link_c[]) linkedValues;
+		Link_c[] allAssocLinks = Link_c.getManyI_LNKsOnR2904((Association_c) value);
+		ArrayList<Link_c> validLinks = new ArrayList<Link_c>();
+		
+		// Create a hashmap for fast search.
+		HashMap<Link_c, String>  DuplicateInspection = new HashMap<Link_c, String>();
+		for (Link_c link : allOriginlinks) {
+			DuplicateInspection.put(link, "");
+		}
+		for (Link_c link : allAssocLinks) {
+			String exist = DuplicateInspection.get(link);
+			if (exist != null){
+				/* A common Link_c instance between instance Link_c array
+				 * and association Link_c array 
+				*/  
+				validLinks.add(link);
+			}
+		}
+		Link_c[] instanceLinks = validLinks.toArray(new Link_c[validLinks.size()]);
+		return instanceLinks;
+	}
     
     public Class getType() {
     	return value.getClass();
