e483e5f8f7efda0e824c099c54b8cdf136e084fe
rmulvey <robert.mulvey@mentor.com>
2015-03-31 20:12:33 -0400

    job: #7519 Removed MGC_EMBEDDED_HOME.  It most places it was jsut not needed, and it places it was used I replaced it with BPHOMEDIR

--- src/installer/Launcher.sh
+++ src/installer/Launcher.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 BPHOMEDIR="C:/mgc/BridgePoint"
-BP_VERSION="4.2.1"
 
 #
 # DO NOT MODIFY ANY OF THE FOLLOWING LINES.
@@ -8,7 +7,6 @@ BP_VERSION="4.2.1"
 export ORIGINAL_PATH=$PATH
 export PATH=$PATH:$BPHOMEDIR/tools/docgen/docbook
 export GDK_NATIVE_WINDOWS=true
-export MGC_EMBEDDED_HOME=$BPHOMEDIR
 export BP_JVM=$BPHOMEDIR/jre/lib/i386/client/libjvm.so
 
 $BPHOMEDIR/eclipse/eclipse -vm $BP_JVM $1 $2 $3 $4 $5 $6 $7 $8 $9
