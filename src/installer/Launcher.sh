a40906bcd2317868f414f0f02ff006392695ec4c
Keith Brown <s.keith.brown@gmail.com>
2015-04-20 19:27:27 -0400

    job #7597 - get rid of PATH manipulations for docbook, they are not needed.  Figure out the launching directory dynamically instead of setting it statically.

--- src/installer/Launcher.sh
+++ src/installer/Launcher.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-BPHOMEDIR="C:/xtuml/BridgePoint"
+# This command gets the location of the directory that holds this script.  We
+# use this to set up the rest of the paths for launching.  If you want to copy
+# this script elsewhere, you should modify BPHOMEDIR to explicitly set the 
+# location where BridgePoint is installed to.
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+BPHOMEDIR="$DIR/.."
 
 #
 # DO NOT MODIFY ANY OF THE FOLLOWING LINES.
 #
-export ORIGINAL_PATH=$PATH
-export PATH=$PATH:$BPHOMEDIR/tools/docgen/docbook
 export GDK_NATIVE_WINDOWS=true
 export BP_JVM=$BPHOMEDIR/jre/lib/i386/client/libjvm.so
 
 $BPHOMEDIR/eclipse/eclipse -vm $BP_JVM $1 $2 $3 $4 $5 $6 $7 $8 $9
 
-# Restore the path
-export PATH=$ORIGINAL_PATH
