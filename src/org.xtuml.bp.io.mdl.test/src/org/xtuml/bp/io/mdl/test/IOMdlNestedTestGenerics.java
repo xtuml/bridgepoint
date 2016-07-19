c420bc2d31dd49ec2644c6e59c765b5ce4bcc0aa
Keith Brown <s.keith.brown@gmail.com>
2016-07-19 17:53:15 -0400

    job #8593 - This test had a lot of junk that was not needed and not being used.  Cleaned this up.

--- src/org.xtuml.bp.io.mdl.test/src/org/xtuml/bp/io/mdl/test/IOMdlNestedTestGenerics.java
+++ src/org.xtuml.bp.io.mdl.test/src/org/xtuml/bp/io/mdl/test/IOMdlNestedTestGenerics.java
@@ -1,9 +1,7 @@
      package org.xtuml.bp.io.mdl.test;
 //=====================================================================
 //
-//File:      $RCSfile: IOMdlNestedTestGenerics.java,v $
-//Version:   $Revision: 1.6 $
-//Modified:  $Date: 2013/05/10 05:13:51 $
+//File:      IOMdlNestedTestGenerics.java
 //
 //(c) Copyright 2004-2014 by Mentor Graphics Corp. All rights reserved.
 //
@@ -21,12 +19,6 @@
 // the License.
 //=====================================================================
 
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IWorkspaceRunnable;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.jface.viewers.TreeViewer;
-import org.eclipse.ui.PlatformUI;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -35,102 +27,27 @@ import org.xtuml.bp.core.Attribute_c;
 import org.xtuml.bp.core.DataType_c;
 import org.xtuml.bp.core.InstanceReferenceDataType_c;
 import org.xtuml.bp.core.ModelClass_c;
-import org.xtuml.bp.core.Ooaofooa;
 import org.xtuml.bp.core.Package_c;
 import org.xtuml.bp.core.SystemModel_c;
-import org.xtuml.bp.core.ui.perspective.BridgePointPerspective;
+import org.xtuml.bp.test.common.BaseTest;
 import org.xtuml.bp.test.common.OrderedRunner;
-import org.xtuml.bp.test.common.TestingUtilities;
-import org.xtuml.bp.ui.canvas.CanvasPlugin;
-import org.xtuml.bp.ui.canvas.Ooaofgraphics;
-import org.xtuml.bp.ui.canvas.test.CanvasTest;
-import org.xtuml.bp.ui.explorer.ExplorerView;
-
-import junit.framework.Assert;
 
 @RunWith(OrderedRunner.class)
-public class IOMdlNestedTestGenerics extends CanvasTest {
+public class IOMdlNestedTestGenerics extends BaseTest {
 
-	static ExplorerView m_bp_view = null;
-	static TreeViewer m_bp_tree = null;
-	static SystemModel_c m_sys = null;
-	String test_id = null;
-	private static boolean generateResults = false;
-	private static boolean initialized = false;
-	static String workspace_path = "";
-    
-
-    
 	public IOMdlNestedTestGenerics() {
 		super("org.xtuml.bp.io.mdl.test", null);
 	}
-	protected String getResultName() {
-		return "Nested" + "_Generics_" + test_id;
-	}	
 
 	@Before
 	public void setUp() throws Exception {
 		super.setUp();
-		if (workspace_path == null || workspace_path.equals(""))
-		{
-			workspace_path = System.getProperty("WORKSPACE_PATH");
-		}
-		assertNotNull( workspace_path );
-		if ( m_wp == null )			
-		{
-			try
-			{
-			  IWorkspaceRunnable r = new IWorkspaceRunnable()
-			  {
-				public void run(IProgressMonitor monitor) throws CoreException
-				{
-					m_wp = PlatformUI.getWorkbench().showPerspective("org.xtuml.bp.core.perspective", PlatformUI.getWorkbench().getActiveWorkbenchWindow());
-					m_bp_view = (ExplorerView)m_wp.findView(BridgePointPerspective.ID_MGC_BP_EXPLORER);
-					m_bp_tree = m_bp_view.getTreeViewer();
-					m_wp.activate(m_bp_view);
-				}
-			  };
-			  IOMdlTestPlugin.getWorkspace().run(r, null);
-			}
-			catch (CoreException x)
-			{
-			  CanvasPlugin.logError("create perspective problem", x);
-			}
-		}
-		if (m_bp_view == null) {
-			PlatformUI.getWorkbench().showPerspective("org.xtuml.bp.core.perspective", PlatformUI.getWorkbench().getActiveWorkbenchWindow());
-			m_bp_view = (ExplorerView)m_wp.findView(BridgePointPerspective.ID_MGC_BP_EXPLORER);
-			m_bp_tree = m_bp_view.getTreeViewer();
-			m_wp.activate(m_bp_view);
-		}
- 
-		if (!initialized)
-		{
-		IProject projectHandle = TestingUtilities.createProject("test");
-		String modelPath = workspace_path + Ooaofooa.MODELS_DIRNAME + "/nested_test." + Ooaofooa.MODELS_EXT;
-		initialized = TestingUtilities.importModelUsingWizard( getSystemModel(projectHandle.getName()), modelPath , true);
-	    modelRoot = Ooaofooa.getInstance(Ooaofooa.createModelRootId(projectHandle, "nested_test", true), true);
-		}
-		graphicsModelRoot = Ooaofgraphics.getInstance(modelRoot.getId());
-	 
 	}
 
 	@After
 	public void tearDown() throws Exception {
 		super.tearDown();
 	}
-	public void setGenerateResults() {
-		try {
-			generateResults = true;
-		} catch (Exception e) {
-			System.out.println(
-				"Exception encountered by test result creator: " + e);
-		}
-
-	}
-	
-
-	
 
 	@Test
 	public void testUpgradeModelWithPublishReference() throws Exception{
@@ -150,7 +67,7 @@ public class IOMdlNestedTestGenerics extends CanvasTest {
 		Attribute_c[] attrs = Attribute_c.getManyO_ATTRsOnR102(testModelClass);
 		for (Attribute_c attribute : attrs) {
 			if ( attribute.getName().equalsIgnoreCase("vehicleReference") || attribute.getName().equalsIgnoreCase("selfReference")){
-				Assert.assertNotNull( InstanceReferenceDataType_c.getOneS_IRDTOnR17(DataType_c.getOneS_DTOnR114(attribute)));
+				assertTrue( "IRDT is null and it should not be.", null != InstanceReferenceDataType_c.getOneS_IRDTOnR17(DataType_c.getOneS_DTOnR114(attribute)));
 			}
 		}
 	}
