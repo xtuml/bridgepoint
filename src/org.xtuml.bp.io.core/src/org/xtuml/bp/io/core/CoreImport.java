1744d335e518daec904d3bc43532688724516676
Levi Starrett <levi@roxsoftware.com>
2017-01-03 11:21:22 -0500

    job #8918 simplified import parser grammars

--- src/org.xtuml.bp.io.core/src/org/xtuml/bp/io/core/CoreImport.java
+++ src/org.xtuml.bp.io.core/src/org/xtuml/bp/io/core/CoreImport.java
@@ -352,12 +352,10 @@ public abstract class CoreImport implements IModelImport {
 
                 SignatureLexer sig_lexer = new SignatureLexer(reader);
                 BodyLexer body_lexer = new BodyLexer(sig_lexer.getInputState());
-                CommentLexer comment_lexer = new CommentLexer(sig_lexer.getInputState());
 
                 TokenStreamSelector selector = new TokenStreamSelector();
                 selector.addInputStream(sig_lexer, "main");
                 selector.addInputStream(body_lexer, "bodylexer");
-                selector.addInputStream(comment_lexer, "commentlexer");
                 selector.select("main");
 
                 pm.beginTask("Reading activity data...", IProgressMonitor.UNKNOWN );
@@ -366,9 +364,9 @@ public abstract class CoreImport implements IModelImport {
 
                 // reset the input buffer
                 actionInputBuffer = null;
-                if (!parser.m_output.isEmpty())
+                if (parser.m_errors || sig_lexer.m_errors || body_lexer.m_errors )
                 {
-                    m_errorMessage = parser.m_output;
+                    m_errorMessage = parser.m_output + sig_lexer.m_output + body_lexer.m_output;
                     pm.done();
                     return false;
                 } else {
