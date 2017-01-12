1df5f23fe9a6b84f2121cce225a37660ba0ea666
robertmulvey <robert.mulvey@onefact.net>
2017-01-12 15:33:12 -0500

    job #8979 Modified the single-file xtuml import wizard to use the ImportHelper to determine if masl was imported. Import helper was modified to set globals that differentiate masl project import from masl domain import. Previously the code did not look for masl_domain. The ModelImportWizard still performs graphics reconciliation if a MASL model was imported. Additionally, now the code was added to perform x2m on masl projects or domains. There is a problem with build dependencies on the bp.x2m plugin that prevent the x2m/Generator.java::{exportProject | exportDomain} from being referenced in the bp.io.mdl plugin. Therefore right now the 2 lines calling those functions are commented out until this build dependency issue is resolved.

--- src/org.xtuml.bp.io.mdl/src/org/xtuml/bp/io/mdl/wizards/ModelImportWizard.java
+++ src/org.xtuml.bp.io.mdl/src/org/xtuml/bp/io/mdl/wizards/ModelImportWizard.java
@@ -49,6 +49,7 @@ import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.PlatformUI;
 import org.xtuml.bp.core.CorePlugin;
 import org.xtuml.bp.core.Ooaofooa;
+import org.xtuml.bp.core.Package_c;
 import org.xtuml.bp.core.SystemModel_c;
 import org.xtuml.bp.core.common.BridgePointPreferencesStore;
 import org.xtuml.bp.core.common.ClassQueryInterface_c;
@@ -201,19 +202,46 @@ public class ModelImportWizard extends Wizard implements IImportWizard {
             ImportHelper helper = new ImportHelper((CoreImport)fImporter);
             helper.resolveMASLproject( fImporter.getLoadedInstances() );
             
-			if (helper.graphicsReconciliationIsNeeded()) {
-				// this must be run on the display thread
-				PlatformUI.getWorkbench().getDisplay().syncExec(
-						new Runnable() {
+			if (helper.maslModelWasImported()) {
+				// Reconcile graphics.
+				PlatformUI.getWorkbench().getDisplay().syncExec(new Runnable() {
+					public void run() {
+						List<NonRootModelElement> systems = new ArrayList<NonRootModelElement>();
+						systems.add(fSystem);
+						GraphicsReconcilerLauncher reconciler = new GraphicsReconcilerLauncher(systems);
+						reconciler.runReconciler(false, true);
+					}
 
+				});				
+
+				// Note that currently xtml files imported from masl models
+				// represent either a masl domain or a project, not both.
+				// below we one or the other if this was a masl model.
+				//
+				// export masl project, if a masl project was imported
+				Package_c maslProject = helper.getImportedMASLProject();
+				if (maslProject != null) {
+					// export project if there was one imported
+					PlatformUI.getWorkbench().getDisplay().syncExec(new Runnable() {
+						public void run() {
+//							Generator.exportProject(maslProject);
+						}
+	
+					});				
+				} else {
+					// export masl donmain if a masl domain was imported
+					Package_c maslDomainsPackage = helper.getImportedMASLDomainPackage();
+					if (maslDomainsPackage != null) {
+						// export domain if there was one imported
+						PlatformUI.getWorkbench().getDisplay().syncExec(new Runnable() {
 							public void run() {
-								List<NonRootModelElement> systems = new ArrayList<NonRootModelElement>();
-								systems.add(fSystem);
-								GraphicsReconcilerLauncher reconciler = new GraphicsReconcilerLauncher(systems);
-								reconciler.runReconciler(false, true);
+								// export all domains in this masl package
+//								Generator.exportDomain(maslDomainsPackage);
 							}
 
 						});
+					}
+				}
 			}
             
 		}
