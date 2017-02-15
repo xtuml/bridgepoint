3dafdfbc2745ef5ddbda8a6461c3c917e5059d88
Jan Koehnlein <jan.koehnlein@typefox.io>
2017-02-15 14:21:56 +0100

    #9174 resynchronize open MASL editors after rename

--- src/org.xtuml.bp.core/src/org/xtuml/bp/core/common/ComponentTransactionListener.java
+++ src/org.xtuml.bp.core/src/org/xtuml/bp/core/common/ComponentTransactionListener.java
@@ -37,13 +37,21 @@ import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IEditorReference;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.texteditor.IDocumentProvider;
+import org.eclipse.ui.texteditor.IDocumentProviderExtension;
+import org.eclipse.xtext.ui.editor.XtextEditor;
 import org.xtuml.bp.core.CorePlugin;
 import org.xtuml.bp.core.DataType_c;
 import org.xtuml.bp.core.Modeleventnotification_c;
 import org.xtuml.bp.core.Ooaofooa;
-import org.xtuml.bp.core.SystemModel_c;
 import org.xtuml.bp.core.ui.PasteAction;
 import org.xtuml.bp.core.util.CoreUtil;
 import org.xtuml.bp.core.util.RenameParticipantUtil;
@@ -258,13 +266,39 @@ public class ComponentTransactionListener implements ITransactionListener {
 				}
 			}
 		}
-		
 		Ooaofooa[] instances = Ooaofooa.getInstances();
 		for(int i = 0; i < instances.length; i++) {
 			instances[i].clearUnreferencedProxies();
 		}
 		IntegrityChecker.startIntegrityChecker(persisted);
+		synchronizeMaslEditors();
 	}
+	
+	private void synchronizeMaslEditors() {
+		Display.getDefault().syncExec( new Runnable() {
+			@Override
+			public void run() {
+				IEditorReference[] editorReferences = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getEditorReferences();
+				for(IEditorReference editorReference: editorReferences) {
+					IEditorPart editor = editorReference.getEditor(false);
+					if(editor instanceof XtextEditor) {
+						IEditorInput editorInput = editor.getEditorInput();
+						IDocumentProvider documentProvider = ((XtextEditor)editor).getDocumentProvider();
+						if(documentProvider instanceof IDocumentProviderExtension) {
+							try {
+								((IDocumentProviderExtension)documentProvider).synchronize(editorInput);
+							} catch(CoreException exc) {
+								CorePlugin.getDefault().getLog().log(
+										new Status(IStatus.ERROR, CorePlugin.getDefault().getBundle().getSymbolicName(), 
+												"Error synchronizing editoras after refactoring", exc));
+							}
+						}
+					}
+				}				
+			}
+		});
+	}
+	
     private IPath[] getFoldersToBeRemoved(PersistableModelComponent pmc) {
     	Collection children = getChildrenOfDomainPMC(pmc);
         IPath[] oldFolders = new IPath[children.size()];
