9fbe24561df1cfaaa6f1ce70725ba4935c637f26
Travis London <travis.london@gmail.com>
2017-05-29 00:50:44 -0700

    Job: #9524 Have Build and Clean launches also build bp.pkg-feature first due to the dependency issues with running maven on individual plug-ins.

--- doc-bridgepoint/process/templates/launch_configs/run_test.sh
+++ doc-bridgepoint/process/templates/launch_configs/run_test.sh
@@ -1,3 +1,7 @@
 # There are some dependency issues with maven
 # This script will run from a test project builder configuration, building org.xtuml.bp.pkg.feature before a test run
-cd ${1}/../src/org.xtuml.bp.pkg-feature && mvn -Dtycho.disableP2Mirrors=true -Dmaven.test.failure.ignore=true install && cd ${2} && mvn -Dtycho.disableP2Mirrors=true -Dmaven.test.failure.ignore=true install
+cmd="install"
+if [[ $# -eq 3 ]] ; then
+  cmd="clean"
+fi
+cd ${1}/../src/org.xtuml.bp.pkg-feature && mvn -Dtycho.disableP2Mirrors=true -Dmaven.test.failure.ignore=true install && cd ${2} && mvn -Dtycho.disableP2Mirrors=true -Dmaven.test.failure.ignore=true $cmd
