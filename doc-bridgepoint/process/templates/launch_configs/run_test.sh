f226159fca253be2228bdb421f243781dde56668
Travis London <travis.london@gmail.com>
2017-05-29 08:22:42 -0700

    Job: #9524 Fix test script to allow use for build/clean/test.  Update workspace build order.

--- doc-bridgepoint/process/templates/launch_configs/run_test.sh
+++ doc-bridgepoint/process/templates/launch_configs/run_test.sh
@@ -1,7 +1,13 @@
 # There are some dependency issues with maven
 # This script will run from a test project builder configuration, building org.xtuml.bp.pkg.feature before a test run
 cmd="install"
+skipTests=-DskipTests
 if [[ $# -eq 3 ]] ; then
-  cmd="clean"
+  # if not test, then we do not want skipTests
+  if [[ $3 = "test" ]]; then
+	skipTests=""
+  else
+    cmd="clean"
+  fi
 fi
-cd ${1}/../src/org.xtuml.bp.pkg-feature && mvn -Dtycho.disableP2Mirrors=true -Dmaven.test.failure.ignore=true install && cd ${2} && mvn -Dtycho.disableP2Mirrors=true -Dmaven.test.failure.ignore=true $cmd
+cd ${1}/../src/org.xtuml.bp.pkg-feature && mvn -Dtycho.disableP2Mirrors=true $skipTests -Dmaven.test.failure.ignore=true install && cd ${2} && mvn -Dtycho.disableP2Mirrors=true $skipTests -Dmaven.test.failure.ignore=true $cmd
