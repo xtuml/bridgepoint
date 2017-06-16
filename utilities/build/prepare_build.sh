9473dd747a47766bcc70ef4e9fe66b00cb295fab
Travis London <travis.london@gmail.com>
2017-06-16 12:32:48 -0700

    Job: #9524 Update prepare_build to use WORKSPACE environment variable

--- utilities/build/prepare_build.sh
+++ utilities/build/prepare_build.sh
@@ -12,12 +12,12 @@ prev_dir=`pwd`
 function importProjects {
   dir=`pwd`
   cd $GIT_DIR
-  java -cp ${bp_install_dir}/plugins/org.eclipse.equinox.launcher_1.3.100.v20150511-1540.jar org.eclipse.equinox.launcher.Main -data ~/workspace -application org.eclipse.cdt.managedbuilder.core.headlessbuild -importAll bridgepoint/src
-  java -cp ${bp_install_dir}/plugins/org.eclipse.equinox.launcher_1.3.100.v20150511-1540.jar org.eclipse.equinox.launcher.Main -data ~/workspace -application org.eclipse.ereDevelopmentWorkspacedt.managedbuilder.core.headlessbuild -importAll bridgepoint/doc-bridgepoint
-  java -cp ${bp_install_dir}/plugins/org.eclipse.equinox.launcher_1.3.100.v20150511-1540.jar org.eclipse.equinox.launcher.Main -data ~/workspace -application org.eclipse.cdt.managedbuilder.core.headlessbuild -importAll bridgepoint/releng
-  java -cp ${bp_install_dir}/plugins/org.eclipse.equinox.launcher_1.3.100.v20150511-1540.jar org.eclipse.equinox.launcher.Main -data ~/workspace -application org.eclipse.cdt.managedbuilder.core.headlessbuild -importAll bridgepoint/utilities
+  java -cp ${bp_install_dir}/plugins/org.eclipse.equinox.launcher_1.3.100.v20150511-1540.jar org.eclipse.equinox.launcher.Main -data $WORKSPACE -application org.eclipse.cdt.managedbuilder.core.headlessbuild -importAll bridgepoint/src
+  java -cp ${bp_install_dir}/plugins/org.eclipse.equinox.launcher_1.3.100.v20150511-1540.jar org.eclipse.equinox.launcher.Main -data $WORKSPACE -application org.eclipse.ereDevelopmentWorkspacedt.managedbuilder.core.headlessbuild -importAll bridgepoint/doc-bridgepoint
+  java -cp ${bp_install_dir}/plugins/org.eclipse.equinox.launcher_1.3.100.v20150511-1540.jar org.eclipse.equinox.launcher.Main -data $WORKSPACE -application org.eclipse.cdt.managedbuilder.core.headlessbuild -importAll bridgepoint/releng
+  java -cp ${bp_install_dir}/plugins/org.eclipse.equinox.launcher_1.3.100.v20150511-1540.jar org.eclipse.equinox.launcher.Main -data $WORKSPACE -application org.eclipse.cdt.managedbuilder.core.headlessbuild -importAll bridgepoint/utilities
   if [ "$INCLUDE_TESTS" == "true" ]; then
-    java -cp ${bp_install_dir}/plugins/org.eclipse.equinox.launcher_1.3.100.v20150511-1540.jar org.eclipse.equinox.launcher.Main -data ~/workspace -application org.eclipse.cdt.managedbuilder.core.headlessbuild -importAll bptest/src
+    java -cp ${bp_install_dir}/plugins/org.eclipse.equinox.launcher_1.3.100.v20150511-1540.jar org.eclipse.equinox.launcher.Main -data $WORKSPACE -application org.eclipse.cdt.managedbuilder.core.headlessbuild -importAll bptest/src
   fi
   cd $dir
 }
@@ -27,21 +27,21 @@ function preBuildProjects {
   if [ "$(uname)" == "Darwin" ];then
     JAVA_ARGS="-XstartOnFirstThread"
   fi
-  java -Xms256m -Xmx1g $JAVA_ARGS -jar ${bp_install_dir}/plugins/org.eclipse.equinox.launcher_*.jar -clean -noSplash -product org.xtuml.bp.pkg.BridgePoint -data ~/workspace -application org.xtuml.bp.cli.Build -project org.xtuml.bp.core -prebuildOnly
-  java -Xms256m -Xmx1g $JAVA_ARGS -jar ${bp_install_dir}/plugins/org.eclipse.equinox.launcher_*.jar -clean -noSplash -product org.xtuml.bp.pkg.BridgePoint -data ~/workspace -application org.xtuml.bp.cli.Build -project org.xtuml.bp.als -prebuildOnly
-  java -Xms256m -Xmx1g $JAVA_ARGS -jar ${bp_install_dir}/plugins/org.eclipse.equinox.launcher_*.jar -clean -noSplash -product org.xtuml.bp.pkg.BridgePoint -data ~/workspace -application org.xtuml.bp.cli.Build -project org.xtuml.bp.ui.canvas -prebuildOnly
+  java -Xms256m -Xmx1g $JAVA_ARGS -jar ${bp_install_dir}/plugins/org.eclipse.equinox.launcher_*.jar -clean -noSplash -product org.xtuml.bp.pkg.BridgePoint -data $WORKSPACE -application org.xtuml.bp.cli.Build -project org.xtuml.bp.core -prebuildOnly
+  java -Xms256m -Xmx1g $JAVA_ARGS -jar ${bp_install_dir}/plugins/org.eclipse.equinox.launcher_*.jar -clean -noSplash -product org.xtuml.bp.pkg.BridgePoint -data $WORKSPACE -application org.xtuml.bp.cli.Build -project org.xtuml.bp.als -prebuildOnly
+  java -Xms256m -Xmx1g $JAVA_ARGS -jar ${bp_install_dir}/plugins/org.eclipse.equinox.launcher_*.jar -clean -noSplash -product org.xtuml.bp.pkg.BridgePoint -data $WORKSPACE -application org.xtuml.bp.cli.Build -project org.xtuml.bp.ui.canvas -prebuildOnly
   if [ "$INCLUDE_TESTS" == "true" ]; then
-      java -Xms256m -Xmx1g $JAVA_ARGS -jar ${bp_install_dir}/plugins/org.eclipse.equinox.launcher_*.jar -clean -noSplash -product org.xtuml.bp.pkg.BridgePoint -data ~/workspace -application org.xtuml.bp.cli.Build -project org.xtuml.bp.core.test -prebuildOnly
+      java -Xms256m -Xmx1g $JAVA_ARGS -jar ${bp_install_dir}/plugins/org.eclipse.equinox.launcher_*.jar -clean -noSplash -product org.xtuml.bp.pkg.BridgePoint -data $WORKSPACE -application org.xtuml.bp.cli.Build -project org.xtuml.bp.core.test -prebuildOnly
   fi
 }
 
 function prepareDevelopmentWorkspace {
   # create the workspace if it does not
   # exist and set it up for development
-  if [ ! -d ~/workspace ]; then
-    mkdir -p ~/workspace/.metadata/.plugins/org.eclipse.core.runtime/.settings
+  if [ ! -d $WORKSPACE ]; then
+    mkdir -p $WORKSPACE/.metadata/.plugins/org.eclipse.core.runtime/.settings
   fi
-  cp -f $GIT_DIR/bridgepoint/utilities/build/preferences/*  ~/workspace/.metadata/.plugins/org.eclipse.core.runtime/.settings
+  cp -f $GIT_DIR/bridgepoint/utilities/build/preferences/*  $WORKSPACE/.metadata/.plugins/org.eclipse.core.runtime/.settings
   cp -f $GIT_DIR/pt_antlr/pt_antlr/antlr.jar  $GIT_DIR/bridgepoint/src/org.xtuml.bp.als/lib/antlr.jar
   echo "Preparing workspace"
 }
