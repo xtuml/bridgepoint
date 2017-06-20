4927957e391ff9c1cb3f0b2689789d958cd5e639
Travis London <travis.london@gmail.com>
2017-06-20 08:08:23 -0700

    Add script dir so that script can be run from any directory

--- utilities/build/prepare_build.sh
+++ utilities/build/prepare_build.sh
@@ -5,7 +5,9 @@
 
 #!/bin/bash
 
-source build_configuration.sh
+SCRIPTPATH=`dirname $0`
+
+source $SCRIPTPATH/build_configuration.sh
 
 prev_dir=`pwd`
 
@@ -60,22 +62,8 @@ if [ "$2" == "-debug" ];then
 fi
 
 cd $dir 
-# do not prepare the workspace if already done
-# here we lazily check for the presence of org.xtuml.bp.core.prefs
-# and antlr.jar
-if [ ! -f $WORKSPACE/.metadata/.plugins/org.eclipse.core.runtime/.settings/org.xtuml.bp.core.prefs ] || [ ! -f $XTUML_DEVELOPMENT_REPOSITORY/src/org.xtuml.bp.als/lib/antlr.jar ]; then
-  prepareDevelopmentWorkspace
-fi
-# do not import projects unless those requiring
-# prebuild are not present
-if [ ! -d $WORKSPACE/.metadata/.plugins/org.eclipse.core.resources/.projects/org.xtuml.bp.core ] ||
-   [ ! -d $WORKSPACE/.metadata/.plugins/org.eclipse.core.resources/.projects/org.xtuml.bp.als ] ||
-   [ ! -d $WORKSPACE/.metadata/.plugins/org.eclipse.core.resources/.projects/org.xtuml.bp.ui.canvas ] ||
-   [ ! -d $WORKSPACE/.metadata/.plugins/org.eclipse.core.resources/.projects/org.xtuml.bp.core ]; then
-  importProjects
-fi
-# always prebuild for now rather than try to fully
-# cover dependencies
+prepareDevelopmentWorkspace
+importProjects
 preBuildProjects
 
 cd $prev_dir
