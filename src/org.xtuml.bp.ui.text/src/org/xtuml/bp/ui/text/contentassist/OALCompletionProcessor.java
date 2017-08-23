41fe67c9c4369965cda1e1331b87a25fa79caf93
Levi Starrett <levi@roxsoftware.com>
2017-08-23 14:00:48 -0400

    job #9571 demo working with operations and attributes

--- src/org.xtuml.bp.ui.text/src/org/xtuml/bp/ui/text/contentassist/OALCompletionProcessor.java
+++ src/org.xtuml.bp.ui.text/src/org/xtuml/bp/ui/text/contentassist/OALCompletionProcessor.java
@@ -9,10 +9,13 @@ import org.eclipse.jface.text.contentassist.ICompletionProposal;
 import org.eclipse.jface.text.contentassist.IContentAssistProcessor;
 import org.eclipse.jface.text.contentassist.IContextInformation;
 import org.eclipse.jface.text.contentassist.IContextInformationValidator;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.ui.IEditorInput;
 import org.xtuml.bp.core.Body_c;
-import org.xtuml.bp.core.ContextAssistanceItem_c;
-import org.xtuml.bp.core.ContextAssistanceList_c;
+import org.xtuml.bp.core.ContentAssistanceItem_c;
+import org.xtuml.bp.core.ContentAssistanceList_c;
+import org.xtuml.bp.core.Contextassistanceitemtypes_c;
+import org.xtuml.bp.core.CorePlugin;
 import org.xtuml.bp.core.common.OALPersistenceUtil;
 import org.xtuml.bp.ui.text.AbstractModelElementPropertyEditorInput;
 import org.xtuml.bp.ui.text.editor.oal.OALEditor;
@@ -24,6 +27,17 @@ public class OALCompletionProcessor implements IContentAssistProcessor {
     public OALCompletionProcessor( OALEditor editor ) {
         this.editor = editor;
     }
+
+    private Image getImage( int type ) {
+        switch( type ) {
+            case Contextassistanceitemtypes_c.Attribute:
+                return CorePlugin.getImageFor( "Attribute_c" );
+            case Contextassistanceitemtypes_c.Operation:
+                return CorePlugin.getImageFor( "Operation_c" );
+            default:
+                return null;
+        }
+    }
     
     @Override
     public ICompletionProposal[] computeCompletionProposals( ITextViewer viewer, int position ) {
@@ -31,12 +45,13 @@ public class OALCompletionProcessor implements IContentAssistProcessor {
         IEditorInput editorInput = editor.getEditorInput();
         if ( editorInput instanceof AbstractModelElementPropertyEditorInput ) {
             Body_c body = OALPersistenceUtil.getOALModelElement(((AbstractModelElementPropertyEditorInput)editorInput).getModelElementContainingProperty());
-            ContextAssistanceList_c list = ContextAssistanceList_c.ContextAssistanceListInstance( body.getModelRoot() );
-            ContextAssistanceItem_c item = ContextAssistanceItem_c.getOneCA_IOnR1601( list );
+            ContentAssistanceList_c list = ContentAssistanceList_c.getOneCA_LOnR1603( body );
+            ContentAssistanceItem_c item = ContentAssistanceItem_c.getOneCA_IOnR1601( list );
             while ( null != item ) {
-                ICompletionProposal proposal = new CompletionProposal( item.getValue(), position, 0, position+item.getValue().length() );
+                ICompletionProposal proposal = new CompletionProposal( item.getReplacement_text(), position, 0, position+item.getReplacement_text().length(),
+                                                                       getImage( item.getType() ), item.getReplacement_text(), null, null );
                 proposals.add( proposal );
-                item = ContextAssistanceItem_c.getOneCA_IOnR1602Precedes( item );
+                item = ContentAssistanceItem_c.getOneCA_IOnR1602Precedes( item );
             }
         }
         return proposals.toArray( new ICompletionProposal[0] );
