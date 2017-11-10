f94c864c445ce69e75a48a359a51d2fd4fc9930f
Levi Starrett <levi@roxsoftware.com>
2017-11-10 11:31:03 -0500

    Job #9761 #9762 implemented findWord with break iterator

--- src/org.xtuml.bp.ui.text/src/org/xtuml/bp/ui/text/editor/BPTextDefaultTextDoubleClickStategy.java
+++ src/org.xtuml.bp.ui.text/src/org/xtuml/bp/ui/text/editor/BPTextDefaultTextDoubleClickStategy.java
@@ -1,8 +1,11 @@
 package org.xtuml.bp.ui.text.editor;
 
+import java.text.BreakIterator;
+
 import org.eclipse.jface.text.DefaultTextDoubleClickStrategy;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IRegion;
+import org.eclipse.jface.text.Region;
 /**
  * This class exists simply to make use of Eclipse's default word find
  * capabilities when double clicking.  An example of use is when using a
@@ -16,7 +19,18 @@ public class BPTextDefaultTextDoubleClickStategy extends DefaultTextDoubleClickS
 
 	@Override
 	public IRegion findWord(IDocument document, int offset) {
-		return super.findWord(document, offset);
+		BreakIterator boundary = BreakIterator.getWordInstance();
+		boundary.setText( document.get() );
+		int start = boundary.isBoundary(offset) ? offset : boundary.preceding(offset);
+		int end = boundary.following(offset);
+		// don't allow the period character in a word
+		int periodIndex = document.get().indexOf( '.', start );
+		if ( periodIndex != -1 && periodIndex < end ) {
+			if ( periodIndex < offset ) start = periodIndex+1;
+			else if ( periodIndex > offset ) end = periodIndex;
+			else { start = periodIndex; end = periodIndex+1; }
+		}
+		return new Region( start, end-start );
 	}
 
 }
