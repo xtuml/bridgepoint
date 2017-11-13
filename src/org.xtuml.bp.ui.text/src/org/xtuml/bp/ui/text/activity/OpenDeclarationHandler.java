6806047cd312ff3548839fd5c5435d797b8664ca
robertmulvey <robert.mulvey@onefact.net>
2017-11-13 14:58:56 -0500

    job #9761 Only enable the Open Declaration CME for ActivityEditor's

--- src/org.xtuml.bp.ui.text/src/org/xtuml/bp/ui/text/activity/OpenDeclarationHandler.java
+++ src/org.xtuml.bp.ui.text/src/org/xtuml/bp/ui/text/activity/OpenDeclarationHandler.java
@@ -3,6 +3,7 @@ package org.xtuml.bp.ui.text.activity;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.PlatformUI;
 
 public class OpenDeclarationHandler extends AbstractHandler {
@@ -10,9 +11,12 @@ public class OpenDeclarationHandler extends AbstractHandler {
 	@Override
 	public Object execute(ExecutionEvent arg0) throws ExecutionException {
 		OpenDeclarationAction action = new OpenDeclarationAction();
-		action.setActiveEditor(null,
-				PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor());
-		action.run(null);
+		IEditorPart editor = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
+		if (editor instanceof ActivityEditor) {
+			action.setActiveEditor(null,
+				editor);
+			action.run(null);
+		}
 		return null;
 	}
 
