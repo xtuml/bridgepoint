ca3971e0568bce309fc6ec5ff0a63499368ce031
Keith Brown <s.keith.brown@gmail.com>
2018-03-02 14:32:34 -0500

    job #9021 - rework interface to underlying file to use static methods

--- src/org.xtuml.bp.core/src/org/xtuml/bp/core/ui/preferences/BridgePointProjectDependenciesPreferences.java
+++ src/org.xtuml.bp.core/src/org/xtuml/bp/core/ui/preferences/BridgePointProjectDependenciesPreferences.java
@@ -1,16 +1,9 @@
 package org.xtuml.bp.core.ui.preferences;
 
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.Vector;
 
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.Path;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.IInputValidator;
 import org.eclipse.jface.dialogs.InputDialog;
@@ -27,12 +20,8 @@ import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.List;
 import org.eclipse.swt.widgets.Text;
-import org.eclipse.ui.IEditorInput;
-import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.PlatformUI;
 import org.osgi.service.prefs.Preferences;
-import org.xtuml.bp.core.CorePlugin;
+import org.xtuml.bp.core.common.DependencyData;
 
 public class BridgePointProjectDependenciesPreferences extends
 		BridgePointProjectPreferences {
@@ -41,34 +30,13 @@ public class BridgePointProjectDependenciesPreferences extends
 	private Button addBtn;
 	private Button editBtn;
 	private Button removeBtn;
-
-	class LinkData {
-		public int index;
-		public String name;
-		public String originalLoc;
-		public String updatedLoc;
-		public IResource resource;
-		public LinkData() { 
-			this.index = -1; 
-			this.name= ""; 
-			this.originalLoc = ""; 
-			this.updatedLoc = ""; 
-			this.resource = null; 
-		}
-		public LinkData(int i, String n, String o, String u, IResource r) { 
-			this.index = i; 
-			this.name = n; 
-			this.originalLoc = o; 
-			this.updatedLoc = u; 
-			this.resource = r; 
-		}
-	}
-
-	private ArrayList<LinkData> links = new ArrayList<LinkData>();
-	private ArrayList<LinkData> linksToRemove = new ArrayList<LinkData>();
 	
+	private IProject project;
+
 	public BridgePointProjectDependenciesPreferences(Preferences projectNode) {
 		super(projectNode);
+		String projectName = projectNode.parent().name();
+	    project = ResourcesPlugin.getWorkspace().getRoot().getProject(projectName);	
 	}
 
 	@Override
@@ -107,7 +75,7 @@ public class BridgePointProjectDependenciesPreferences extends
 		IInputValidator validator = new IInputValidator() {
 			public String isValid(String newText) {
 				if (newText.contains("\\") || newText.contains(":") ) {
-					// TODO - this isn't a full solution to this problem, just a placeholder to put a better solution.
+					// NOTE - this isn't a full solution to this problem, just a placeholder to put a better solution.
 					// see https://stackoverflow.com/questions/468789/is-there-a-way-in-java-to-determine-if-a-path-is-valid-without-attempting-to-cre
 					return "Invalid path characters specified.";
 				} 
@@ -131,7 +99,6 @@ public class BridgePointProjectDependenciesPreferences extends
 				if (dialog.open() == InputDialog.OK) {
 					String updatedLoc = dialog.getValue();
 					dependencyListCtrl.add(updatedLoc);
-					links.add(new LinkData(dependencyListCtrl.getItemCount()-1,"","",updatedLoc,null));
 				}
 			}
 		});
@@ -144,7 +111,6 @@ public class BridgePointProjectDependenciesPreferences extends
 					if (dialog.open() == InputDialog.OK) {
 						String updatedLoc = dialog.getValue();
 						dependencyListCtrl.setItem(index, updatedLoc);
-						links.get(index).updatedLoc = updatedLoc;
 					}
 				}
 			}
@@ -154,8 +120,6 @@ public class BridgePointProjectDependenciesPreferences extends
 	        	if (dependencyListCtrl.getSelectionIndex() >= 0) {
 	        		int i = dependencyListCtrl.getSelectionIndex();
 	        		dependencyListCtrl.remove(i);
-					linksToRemove.add(new LinkData(-1,"","","",links.get(i).resource));
-	        		links.remove(i);
 	        	}
 	        }
 	    });
@@ -206,7 +170,11 @@ public class BridgePointProjectDependenciesPreferences extends
 		Label ploc = new Label(envGroup, SWT.NONE );
 		ploc.setText("PROJECT_LOC");
 		Text plocval = new Text(envGroup, SWT.READ_ONLY | SWT.BORDER);
-		plocval.setText(getCurrentProject().getLocation().toOSString());
+		if (project != null) {
+			plocval.setText(project.getLocation().toOSString());
+		} else {
+			plocval.setText("ERROR! No project selected.");
+		}
 		plocval.setLayoutData(infoGridData);
 		
 		envGroup.pack();
@@ -227,115 +195,15 @@ public class BridgePointProjectDependenciesPreferences extends
 
 	@Override
 	protected void syncUIWithPreferences() {
-		// Read links from the project models/ folder and populate them into the list
-		IProject project = getCurrentProject();
-		if ( project != null ) {
-			IFolder modelsDir = project.getFolder("models");
-			if ( modelsDir != null ) {
-				try {
-					IResource[] members = modelsDir.members();
-					int i = 0;
-					for ( IResource member : members ) {
-						if ( member.isLinked() ) {
-							String loc = getEditableLocationText(member);
-							links.add(new LinkData(i, member.getName(), loc, "", member));
-							dependencyListCtrl.add(loc);
-							i = i + 1;
-						} 
-					}
-				} catch (CoreException ce) {
-					CorePlugin.logError("Error finding linked dependencies.", ce);
-				}
-			}
+		Vector<String> curDeps = DependencyData.getRawDependencies(project);
+		for (String curDep : curDeps) {
+			dependencyListCtrl.add(curDep);
 		}
 	}
 
 	@Override
 	protected void syncPreferencesWithUI() {
-		// TODO - SKB - finish
-		// Iterate through the link data. If we have updated values, persist them.
-		Iterator iter = links.iterator();
-		while (iter.hasNext()) {
-			IFile newFile;
-			/*
-             IFile newFile = project.getFile(workspacePath);
-             newFile.createLink(actualPath, flags, null);
-
-             IFolder newFolder = project.getFolder(workspacePath);
-             newFolder.createLink(actualPath, flags, null);
-			 */
-		}
-			
-		//	String depLoc = getPersistableLocationText(dependency);
-		
-		// Iterate through the linksToRemove and delete them
-		// TODO - CAREFUL!  Make sure we don't delete stuff we don't want to.
-		/*iter = linksToRemove.iterator();
-		while (iter.hasNext()) {
-			LinkData l = (LinkData) iter.next();
-			try {
-				l.resource.delete(true, null);
-			} catch (CoreException ce) {
-				CorePlugin.logError("Error finding linked dependencies.", ce);
-			}
-		}*/
-		/* TODO - second deletion option
-		for ( LinkData ld : linksToRemove ) {
-			try {
-				ld.resource.delete(true, null);
-			} catch (CoreException ce) {
-				CorePlugin.logError("Error finding linked dependencies.", ce);
-			}	
-		}
-		*/
+		DependencyData.persist(project, dependencyListCtrl.getItems());
 	}
 
-	// TODO - maybe move to ProjectUtilities
-	private IProject getCurrentProject() {
-		IProject project = null;
-		IWorkbenchPage activePage = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
-
-		IEditorPart activeEditor = activePage.getActiveEditor();
-
-		if (activeEditor != null) {
-		   IEditorInput input = activeEditor.getEditorInput();
-
-		   project = input.getAdapter(IProject.class);
-		   if (project == null) {
-		      IResource resource = input.getAdapter(IResource.class);
-		      if (resource != null) {
-		         project = resource.getProject();
-		      }
-		   }
-		}
-		return project;
-	}
-
-	private String getEditableLocationText(IResource resource) {
-		URI resolvedLocation = resource.getLocationURI();
-		URI location = resolvedLocation;
-		boolean isLinked = resource.isLinked();
-		if (isLinked) {
-			location = resource.getRawLocationURI();
-		}
-		if (location == null) {
-			return "Invalid path";
-		}
-
-		if (resolvedLocation.getScheme() == null) {
-			return location.toString();
-		}
-
-		if (isLinked) {
-			Path p = new Path(resource.getRawLocationURI().getPath());
-			String rVal = resource.getPathVariableManager().convertToUserEditableFormat(p.toOSString(), true);
-			return rVal;
-		}
-		return location.toString();
-	}
-
-	private String getPersistableLocationText(String original) {
-		String rVal = getCurrentProject().getPathVariableManager().convertFromUserEditableFormat(original, true);
-		return rVal;
-	}
 }
