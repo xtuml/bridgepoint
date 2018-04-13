dbc5e6d4d10f68c0d07bfda1481e5880c7e6c625
Levi Starrett <levi@roxsoftware.com>
2018-04-13 11:12:18 -0400

    job #10202 added mechanism for setting default workspace preferences based on action language dialect

--- src/org.xtuml.bp.welcome/src/org/xtuml/bp/welcome/gettingstarted/SampleProjectGettingStartedAction.java
+++ src/org.xtuml.bp.welcome/src/org/xtuml/bp/welcome/gettingstarted/SampleProjectGettingStartedAction.java
@@ -38,9 +38,11 @@ import org.eclipse.ui.intro.IIntroSite;
 import org.eclipse.ui.intro.config.IIntroAction;
 import org.eclipse.ui.part.FileEditorInput;
 import org.osgi.service.prefs.Preferences;
+import org.xtuml.bp.core.Actiondialect_c;
 import org.xtuml.bp.core.CorePlugin;
 import org.xtuml.bp.core.Ooaofooa;
 import org.xtuml.bp.core.SystemModel_c;
+import org.xtuml.bp.core.common.BridgePointPreferencesModel;
 import org.xtuml.bp.core.ui.preferences.BridgePointProjectPreferences;
 import org.xtuml.bp.core.util.UIUtil;
 import org.xtuml.bp.core.util.WorkspaceUtil;
@@ -57,16 +59,17 @@ public class SampleProjectGettingStartedAction implements IIntroAction {
         String launchGettingStartedHelp = params.getProperty("LaunchGettingStartedHelp", "true");
         String readmePath = params.getProperty("readmePath", "");
         String exes = params.getProperty("exes", "");
+        String dialect = params.getProperty("dialect", "OAL");
         if (modelName.equals("load_error")) {
                CorePlugin.logError(
                     "The SampleProjectGettingStartedAction could not determine the model requested.", null);
                return;
         } else {
-            setup(modelName, singleFile, enableIPR, importIntoWorkspace, launchGettingStartedHelp, readmePath, exes);
+            setup(modelName, singleFile, enableIPR, importIntoWorkspace, launchGettingStartedHelp, readmePath, exes, dialect);
         }
     }
 
-    private void setup(String modelName, String singleFile, String enableIPR, String enableImportIntoWorkspace, String launchGettingStarted, String readmePath, String exes) {
+    private void setup(String modelName, String singleFile, String enableIPR, String enableImportIntoWorkspace, String launchGettingStarted, String readmePath, String exes, String dialect) {
         // create project and all necessary parts
         if ( setupProject(modelName, singleFile, enableIPR, enableImportIntoWorkspace) ) {
             // show the xtUML Modeling perspective if not shown
@@ -74,9 +77,12 @@ public class SampleProjectGettingStartedAction implements IIntroAction {
             // close welcome page for the xtUML perspective
 
             closeWelcomePage();
-            
+
             // if there are executables, assure their permissions are set properly
             setExePermissions( exes );
+
+            // check for dialect specific workspace settings
+            checkWorkspaceSettings( dialect );
 
             if (singleFile.compareToIgnoreCase("true") != 0)  {
                 // See if there is a readme file and if so open it
@@ -121,8 +127,8 @@ public class SampleProjectGettingStartedAction implements IIntroAction {
                 CorePlugin.logError(
                         "Could not disable Auto Building", e1);
             }
- 
-            String modelPath; 
+
+            String modelPath;
             if (singleFile.compareToIgnoreCase("true") == 0) {
                 project = ProjectUtilities.createProject(modelName);
                 SystemModel_c systemModel = ProjectUtilities.getSystemModel(project);
@@ -134,7 +140,7 @@ public class SampleProjectGettingStartedAction implements IIntroAction {
                     Preferences projectNode = projectScope.getNode(BridgePointProjectPreferences.BP_PROJECT_PREFERENCES_ID);
                     projectNode.putBoolean("com.mentor.nucleus.bp.ui.project.references", true);
                 }
-            
+
                 modelPath = resolvePath(IGettingStartedConstants.modelFolder
                         + "/" + modelName + "." + Ooaofooa.MODELS_EXT); //$NON-NLS-1$ //$NON-NLS-2$
                 if (!modelPath.isEmpty()) {
@@ -165,7 +171,7 @@ public class SampleProjectGettingStartedAction implements IIntroAction {
 
         if (!setupSucceeded) {
             IStatus status = new Status(IStatus.ERROR, SampleProjectGettingStartedAction.class
-                    .getPackage().getName(), IStatus.ERROR, 
+                    .getPackage().getName(), IStatus.ERROR,
                     "Failed to create the sample project: " + modelName, null);
             CoreException ce = new CoreException(status);
             CorePlugin.logError(
@@ -209,7 +215,7 @@ public class SampleProjectGettingStartedAction implements IIntroAction {
      * Open the file named README if it exists for the given project name.
      * If 'readmePath' contains a value, attempt to open the file that it points
      * to.
-     * 
+     *
      * @param modelFolderName
      * @param readmePath
      */
@@ -235,30 +241,90 @@ public class SampleProjectGettingStartedAction implements IIntroAction {
             CorePlugin.logError("Failed to open " + modelFolderName + " README.", pie);
         }
     }
-    
+
     /**
      * Mark each file in exeString as executable. exeString is a comma
      * separated list of files. If exeString is null or empty, or the file does
      * not exist in the workspace, silently do nothing.
-     * 
+     *
      * @param exeString
      */
     private void setExePermissions( String exeString ) {
-    	if ( null != exeString ) {
-    	    String[] exes = exeString.split( "," );
-    	    for ( String exe : exes ) {
-    	    	IFile exeFile = ResourcesPlugin.getWorkspace().getRoot().getFile( new Path( exe ) );
-    	    	if ( exeFile.exists() ) {
-    	    		ResourceAttributes attributes = exeFile.getResourceAttributes();
-    	    		attributes.setExecutable( true );
-    	    		try {
-						exeFile.setResourceAttributes( attributes );
-					} catch (CoreException e) {
-						CorePlugin.logError( "Error setting file executable.", e);
-					}
-    	    	}
-    	    }
-    	}
+        if ( null != exeString ) {
+            String[] exes = exeString.split( "," );
+            for ( String exe : exes ) {
+                IFile exeFile = ResourcesPlugin.getWorkspace().getRoot().getFile( new Path( exe ) );
+                if ( exeFile.exists() ) {
+                    ResourceAttributes attributes = exeFile.getResourceAttributes();
+                    attributes.setExecutable( true );
+                    try {
+                        exeFile.setResourceAttributes( attributes );
+                    } catch (CoreException e) {
+                        CorePlugin.logError( "Error setting file executable.", e);
+                    }
+                }
+            }
+        }
     }
-    
+
+    /**
+     * Check for workspace specific settings. Open a dialog asking the user if he would
+     * like these settings to be set automatically. Also warn that mixing dialects is not
+     * recommended.
+     *
+     * @param dialect
+     */
+    private void checkWorkspaceSettings( String dialect ) {
+        if ( null != dialect ) {
+            BridgePointPreferencesModel model = new BridgePointPreferencesModel();
+            model.getStore().loadModel( CorePlugin.getDefault().getPreferenceStore(), null, model );
+            final String title = "Set default " + dialect + " preferences";
+            final String message = "Users working with " + dialect + " dialect models are required to set the following workspace preferences. " +
+                                   "Select OK to set these preferences now.\n\n" +
+                                   "Note: it is not recommended to edit models with different action language dialects in the same workspace.";
+            if ( "OAL".equals( dialect ) ) {
+                // Check:
+                // - default dialect == OAL
+                // - concrete polys == disabled
+                if ( model.defaultActionLanguageDialect != Actiondialect_c.oal ||
+                     model.allowConcretePolys == true ) {
+                    boolean setPreferences = UIUtil.openScrollableTextDialog( PlatformUI.getWorkbench().getDisplay().getActiveShell(), true,
+                        title,
+                        "Default action language dialect: OAL\n" +
+                        "Allow concrete polymorphic events: false",
+                        message,
+                        null, null, true, false);
+                    if ( setPreferences ) {
+                        model.defaultActionLanguageDialect = Actiondialect_c.oal;
+                        model.allowConcretePolys = false;
+                    }
+                }
+            }
+            else if ( "MASL".equals( dialect ) ) {
+                // Check:
+                // - default dialect == MASL
+                // - concrete polys == enabled
+                // - restricted identifiers == enabled
+                if ( model.defaultActionLanguageDialect != Actiondialect_c.masl ||
+                     model.allowConcretePolys == false ||
+                     model.requireMaslStyleIdentifiers == false ) {
+                    boolean setPreferences = UIUtil.openScrollableTextDialog( PlatformUI.getWorkbench().getDisplay().getActiveShell(), true,
+                        title,
+                        "Default action language dialect: MASL\n" +
+                        "Enable restricted identifier naming for model elements: true\n" +
+                        "Allow concrete polymorphic events: true",
+                        message,
+                        null, null, true, false);
+                    if ( setPreferences ) {
+                        model.defaultActionLanguageDialect = Actiondialect_c.masl;
+                        model.requireMaslStyleIdentifiers = true;
+                        model.allowConcretePolys = true;
+                    }
+                }
+            }
+            else { /* do nothing */ }
+            model.getStore().saveModel( CorePlugin.getDefault().getPreferenceStore(), model );
+        }
+    }
+
 }
