0ff62bab0b3d5acb9fdc22e6903ab688a9b21de0
Keith Brown <s.keith.brown@gmail.com>
2018-12-13 15:38:57 -0500

    job #8058 - remove pointless code

--- src/org.xtuml.bp.integrity/src/org/xtuml/bp/integrity/actions/checkReferentialIntegrityAction.java
+++ src/org.xtuml.bp.integrity/src/org/xtuml/bp/integrity/actions/checkReferentialIntegrityAction.java
@@ -13,15 +13,12 @@ package org.xtuml.bp.integrity.actions;
 
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
-
 import org.xtuml.bp.integrity.generator.Generator;
 
 public class checkReferentialIntegrityAction implements IObjectActionDelegate {
 
-	private Object curSel = null;
 	@Override
 	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
 	}
@@ -33,11 +30,6 @@ public class checkReferentialIntegrityAction implements IObjectActionDelegate {
 
 	@Override
 	public void selectionChanged(IAction action, ISelection selection) {
-		if (selection instanceof IStructuredSelection) {
-			IStructuredSelection sSel = (IStructuredSelection)selection;
-			Object selected = sSel.getFirstElement();
-		}
-
 	}
 
 }
