8984aee3f8cafbf2e4a4cbbda7dc2043b4c10c2d
Levi Starrett <levi@roxsoftware.com>
2019-01-21 09:40:23 -0500

    job #11461 adjust so project setup works

--- src/org.xtuml.bp.mc.masl/src/org/xtuml/bp/mc/masl/MCNature.java
+++ src/org.xtuml.bp.mc.masl/src/org/xtuml/bp/mc/masl/MCNature.java
@@ -1,8 +1,18 @@
 package org.xtuml.bp.mc.masl;
 
-import org.eclipse.core.resources.IProject;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+import org.eclipse.core.resources.ICommand;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.xtuml.bp.mc.AbstractActivator;
 import org.xtuml.bp.mc.AbstractNature;
+import org.xtuml.bp.utilities.build.BuilderManagement;
 
 public class MCNature extends AbstractNature {
     // The shared instance
@@ -23,6 +33,45 @@ public class MCNature extends AbstractNature {
         super(Activator.getDefault(), EXPORT_BUILDER_ID);
         singleton = this;
     }
+
+    @Override
+    public void configure() throws CoreException {
+        // Add required folders
+        IFolder genFolder = getProject().getFolder(AbstractActivator.GEN_FOLDER_NAME);
+        createFolderIfNonexistent(genFolder);
+
+        // Configure builders
+        configureBuilders();
+
+        // refresh directory to pick up new files
+        try {
+            getProject().refreshLocal(IResource.DEPTH_INFINITE, null);
+        } catch (CoreException e) {
+            abstractActivator.logError("During refresh while adding nature", e);
+        }
+    }
+
+    private void configureBuilders() throws CoreException {
+        // Get project description and then the associated build commands
+        IProjectDescription desc = getProject().getDescription();
+        List<ICommand> commands = new ArrayList<>(Arrays.asList(desc.getBuildSpec()));
+        
+        // add builder to project
+        ICommand command = desc.newCommand();
+        command.setBuilderName(MaslExportBuilder.BUILDER_ID);
+        commands.add(0, command);
+
+        // Determine if pre-builder already associated
+        if (BuilderManagement.hasBuilder(getProject(), EXPORT_BUILDER_ID) == -1) {
+            command = desc.newCommand();
+            command.setBuilderName(EXPORT_BUILDER_ID);
+            commands.add(0, command);
+        }
+        desc.setBuildSpec(commands.toArray(new ICommand[0]));
+        getProject().setDescription(desc, null);
+    }
+
+
 
     /**
      * Returns the shared instance. Creates if it has not yet been created
--- src/org.xtuml.bp.mc.masl/src/org/xtuml/bp/mc/masl/MaslExportBuilder.java
+++ src/org.xtuml.bp.mc.masl/src/org/xtuml/bp/mc/masl/MaslExportBuilder.java
@@ -8,7 +8,9 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 
 public class MaslExportBuilder extends IncrementalProjectBuilder {
-
+    
+    public static final String BUILDER_ID = "org.xtuml.bp.mc.masl.masl_builder";
+    
     @Override
     protected IProject[] build(int kind, Map<String, String> args, IProgressMonitor monitor) throws CoreException {
         System.out.println("LEVI building masl");
