fd980f959733a744682dcc5b75c6a3fd86915921
Keith Brown <s.keith.brown@gmail.com>
2019-07-24 02:01:30 -0400

    job #11704 - updating version

--- utilities/build/build_configuration.sh
+++ utilities/build/build_configuration.sh
@@ -1,13 +1,13 @@
 # This script sets up environment variables that
 # are required to prepare a development workspace
 # as well as build such a workspace
-export BP_GIT_DIR=~/git
+export BP_GIT_DIR=~/git2
 # for mac us this install dir
 export bp_install_dir=~/xtuml/BridgePoint.app/Contents/Eclipse/
 # for linux uncomment this line and comment the mac version
 #export bp_install_dir=~/xtuml/BridgePoint
-export WORKSPACE=~/workspace
-export INCLUDE_TESTS=false
+export WORKSPACE=~/ws/dev2
+export INCLUDE_TESTS=true
 export XTUML_DEVELOPMENT_REPOSITORY=${BP_GIT_DIR}/bridgepoint
 export XTUML_TEST_MODEL_REPOSITORY=${BP_GIT_DIR}/models/test
 export mcj_path=${XTUML_DEVELOPMENT_REPOSITORY}/src/MC-Java
