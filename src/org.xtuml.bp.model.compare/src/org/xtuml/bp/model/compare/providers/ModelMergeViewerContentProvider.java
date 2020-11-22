5fe538013a5db624734fd8ac4df41d199865818e
tlondon-techmod <travis.london@gmail.com>
2020-11-22 10:49:47 -0500

    Job: #11761 Add standard headers

--- src/org.xtuml.bp.model.compare/src/org/xtuml/bp/model/compare/providers/ModelMergeViewerContentProvider.java
+++ src/org.xtuml.bp.model.compare/src/org/xtuml/bp/model/compare/providers/ModelMergeViewerContentProvider.java
@@ -1,5 +1,26 @@
 package org.xtuml.bp.model.compare.providers;
-
+//========================================================================
+//
+//File: providers/ModelMergeViewerContentProvider.java
+//
+//This work is licensed under the Creative Commons CC0 License
+//
+//========================================================================
+//Licensed under the Apache License, Version 2.0 (the "License"); you may not 
+//use this file except in compliance with the License.  You may obtain a copy 
+//of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software 
+//distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
+//WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   See the 
+//License for the specific language governing permissions and limitations under
+//the License.
+//======================================================================== 
+//
+// Provider handling left/right content as well as writing data to either.
+//
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
