6d3a59f995ee792001b8fb5928bfbe41ba0a1b98
Travis London <travis.london@gmail.com>
2021-12-23 06:19:33 -0500

    Working on rename handling

--- src/org.xtuml.bp.ui.canvas/src/org/xtuml/bp/ui/canvas/persistence/WriteTransactionListener.java
+++ src/org.xtuml.bp.ui.canvas/src/org/xtuml/bp/ui/canvas/persistence/WriteTransactionListener.java
@@ -1,9 +1,14 @@
 package org.xtuml.bp.ui.canvas.persistence;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Stream;
 
+import org.xtuml.bp.core.Modeleventnotification_c;
+import org.xtuml.bp.core.Ooaofooa;
+import org.xtuml.bp.core.common.AttributeChangeModelDelta;
 import org.xtuml.bp.core.common.IModelDelta;
 import org.xtuml.bp.core.common.ITransactionListener;
 import org.xtuml.bp.core.common.NonRootModelElement;
@@ -14,13 +19,14 @@ import org.xtuml.bp.ui.canvas.Ooaofgraphics;
 
 public class WriteTransactionListener implements ITransactionListener {
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public void transactionEnded(Transaction transaction) {
 		PersistenceExtensionRegistry persistenceExtensionRegistry = CanvasPlugin.getDefault()
 				.getPersistenceExtensionRegistry();
 		persistenceExtensionRegistry.registeredExtensions.stream().forEach(pe -> {
 			IGraphicalWriter writer = pe.getWriter();
-			// we are interested in Ooaofgraphics changes only
+			// we are interested in Ooaofgraphics changes
 			Set<PersistableModelComponent> collectedComponents = new HashSet<>();
 			IModelDelta[] deltas = transaction.getDeltas(Ooaofgraphics.getDefaultInstance());
 			if (deltas != null) {
@@ -31,10 +37,36 @@ public class WriteTransactionListener implements ITransactionListener {
 						collectedComponents.add(pmc);
 					}
 				});
-				collectedComponents.forEach(component -> {
-					writer.write(component.getRootModelElement());
+			}
+			// and core element renames, as any persistence will have to rely
+			// on the element path for resolution
+			Map<AttributeChangeModelDelta, PersistableModelComponent> renamedComponents = new HashMap<>();
+			IModelDelta[] coreDeltas = transaction.getDeltas(Ooaofooa.getDefaultInstance());
+			if (coreDeltas != null) {
+				Stream.of(coreDeltas).filter(d -> d.getKind() == Modeleventnotification_c.DELTA_ATTRIBUTE_CHANGE && ((AttributeChangeModelDelta) d).getAttributeName().equals("Name")).forEach(delta -> {
+					PersistableModelComponent pmc = ((NonRootModelElement) delta.getModelElement())
+							.getPersistableComponent();
+					renamedComponents.put((AttributeChangeModelDelta) delta, pmc);
+					pmc.getChildren().forEach(child -> {
+						addPmcAndChildren(collectedComponents, pmc);
+					});
 				});
 			}
+			// handle renames first
+			renamedComponents.entrySet().stream().forEach(entry -> {
+				writer.write(entry.getValue().getRootModelElement(), entry.getKey());
+			});
+			collectedComponents.forEach(component -> {
+				writer.write(component.getRootModelElement());
+			});
+		});
+	}
+
+	@SuppressWarnings("unchecked")
+	private void addPmcAndChildren(Set<PersistableModelComponent> collection, PersistableModelComponent pmc) {
+		collection.add(pmc);
+		pmc.getChildren().forEach(child -> {
+			addPmcAndChildren(collection, (PersistableModelComponent) child);
 		});
 	}
 }
