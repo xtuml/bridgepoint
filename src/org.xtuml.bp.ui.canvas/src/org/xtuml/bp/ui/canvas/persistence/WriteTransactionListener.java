31652f28917645b0542ba8306b1fd06b55266d73
Travis London <travis.london@fmay.net>
2021-12-26 08:00:18 -0500

    Needs to respond to some association changes, for instance component ref assignment

--- src/org.xtuml.bp.ui.canvas/src/org/xtuml/bp/ui/canvas/persistence/WriteTransactionListener.java
+++ src/org.xtuml.bp.ui.canvas/src/org/xtuml/bp/ui/canvas/persistence/WriteTransactionListener.java
@@ -4,10 +4,14 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Stream;
 
+import org.xtuml.bp.core.ComponentReference_c;
+import org.xtuml.bp.core.InterfaceReference_c;
+import org.xtuml.bp.core.Ooaofooa;
 import org.xtuml.bp.core.common.IModelDelta;
 import org.xtuml.bp.core.common.ITransactionListener;
 import org.xtuml.bp.core.common.NonRootModelElement;
 import org.xtuml.bp.core.common.PersistableModelComponent;
+import org.xtuml.bp.core.common.RelationshipChangeModelDelta;
 import org.xtuml.bp.core.common.Transaction;
 import org.xtuml.bp.ui.canvas.CanvasPlugin;
 import org.xtuml.bp.ui.canvas.Ooaofgraphics;
@@ -32,10 +36,44 @@ public class WriteTransactionListener implements ITransactionListener {
 					}
 				});
 			}
+			// some ooaofooa changes modify the path link to elements
+			IModelDelta[] coreDeltas = transaction.getDeltas(Ooaofooa.getDefaultInstance());
+			if (coreDeltas != null) {
+				Stream.of(coreDeltas).forEach(delta -> {
+					if (delta instanceof RelationshipChangeModelDelta) {
+						// look at component assignments, as they change the resolution
+						// path
+						boolean triggersWrite = false;
+						NonRootModelElement modelElement = null;
+						RelationshipChangeModelDelta rcmd = (RelationshipChangeModelDelta) delta;
+						if (rcmd.getModelElement() instanceof ComponentReference_c) {
+							if (rcmd.getRelationName().equals("4201")) {
+								triggersWrite = true;
+								modelElement = (NonRootModelElement) delta.getModelElement();
+							}
+						}
+						// look at interface ref formalization
+						if (rcmd.getModelElement() instanceof InterfaceReference_c) {
+							if (rcmd.getRelationName().equals("4012")) {
+								triggersWrite = true;
+								// we need the component container for graphics
+								modelElement = ((NonRootModelElement) delta.getModelElement()).getPersistableComponent()
+										.getParent().getRootModelElement();
+							}
+						}
+						if (triggersWrite) {
+							PersistableModelComponent pmc = modelElement.getPersistableComponent();
+							if (pmc != null) {
+								collectedComponents.add(pmc);
+							}
+						}
+					}
+				});
+			}
 			collectedComponents.forEach(component -> {
 				writer.write(component.getRootModelElement());
 			});
 		});
 	}
-	
+
 }
