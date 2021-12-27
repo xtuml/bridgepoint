b6f4e22409634acb0301bcb7bf158d6dd0013b53
Travis London <travis.london@fmay.net>
2021-12-27 09:37:58 -0500

    Need to write graphics on certain core changes

--- src/org.xtuml.bp.ui.canvas/src/org/xtuml/bp/ui/canvas/persistence/WriteTransactionListener.java
+++ src/org.xtuml.bp.ui.canvas/src/org/xtuml/bp/ui/canvas/persistence/WriteTransactionListener.java
@@ -4,9 +4,22 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Stream;
 
+import org.xtuml.bp.core.Association_c;
+import org.xtuml.bp.core.AsynchronousMessage_c;
+import org.xtuml.bp.core.ClassInstanceParticipant_c;
+import org.xtuml.bp.core.ClassParticipant_c;
+import org.xtuml.bp.core.CommunicationLink_c;
+import org.xtuml.bp.core.ComponentParticipant_c;
 import org.xtuml.bp.core.ComponentReference_c;
+import org.xtuml.bp.core.CreationTransition_c;
+import org.xtuml.bp.core.ExternalEntityParticipant_c;
 import org.xtuml.bp.core.InterfaceReference_c;
 import org.xtuml.bp.core.Ooaofooa;
+import org.xtuml.bp.core.PackageParticipant_c;
+import org.xtuml.bp.core.ReturnMessage_c;
+import org.xtuml.bp.core.SynchronousMessage_c;
+import org.xtuml.bp.core.Transition_c;
+import org.xtuml.bp.core.common.AttributeChangeModelDelta;
 import org.xtuml.bp.core.common.IModelDelta;
 import org.xtuml.bp.core.common.ITransactionListener;
 import org.xtuml.bp.core.common.NonRootModelElement;
@@ -49,7 +62,6 @@ public class WriteTransactionListener implements ITransactionListener {
 						if (rcmd.getModelElement() instanceof ComponentReference_c) {
 							if (rcmd.getRelationName().equals("4201")) {
 								triggersWrite = true;
-								modelElement = (NonRootModelElement) delta.getModelElement();
 							}
 						}
 						// look at interface ref formalization
@@ -61,7 +73,92 @@ public class WriteTransactionListener implements ITransactionListener {
 										.getParent().getRootModelElement();
 							}
 						}
+						// look at participant formalization
+						if (rcmd.getModelElement() instanceof PackageParticipant_c) {
+							if (rcmd.getRelationName().equals("956")) {
+								triggersWrite = true;
+							}
+						}
+						if (rcmd.getModelElement() instanceof ClassParticipant_c) {
+							if (rcmd.getRelationName().equals("939")) {
+								triggersWrite = true;
+							}
+						}
+						if (rcmd.getModelElement() instanceof ClassInstanceParticipant_c) {
+							if (rcmd.getRelationName().equals("934")) {
+								triggersWrite = true;
+							}
+						}
+						if (rcmd.getModelElement() instanceof ComponentParticipant_c) {
+							if (rcmd.getRelationName().equals("955")) {
+								triggersWrite = true;
+							}
+						}
+						if (rcmd.getModelElement() instanceof ExternalEntityParticipant_c) {
+							if (rcmd.getRelationName().equals("933")) {
+								triggersWrite = true;
+							}
+						}
+						// look at event assignment
+						if (rcmd.getDestinationModelElement() instanceof Transition_c) {
+							if (rcmd.getRelationName().equals("507")) {
+								triggersWrite = true;
+							}
+						}
+						if (rcmd.getModelElement() instanceof CreationTransition_c) {
+							if (rcmd.getRelationName().equals("509")) {
+								triggersWrite = true;
+							}
+						}
+						if (rcmd.getModelElement() instanceof SynchronousMessage_c) {
+							if (rcmd.getRelationName().equals("1020")) {
+								triggersWrite = true;
+							}
+						}
+						if (rcmd.getModelElement() instanceof AsynchronousMessage_c) {
+							if (rcmd.getRelationName().equals("1019")) {
+								triggersWrite = true;
+							}
+						}
 						if (triggersWrite) {
+							if (modelElement == null) {
+								modelElement = (NonRootModelElement) delta.getModelElement();
+							}
+							PersistableModelComponent pmc = modelElement.getPersistableComponent();
+							if (pmc != null) {
+								collectedComponents.add(pmc);
+							}
+						}
+					}
+					if (delta instanceof AttributeChangeModelDelta) {
+						boolean triggersWrite = false;
+						NonRootModelElement modelElement = null;
+						// communication link name change will create a path element
+						// we need to write it on first name change
+						AttributeChangeModelDelta attrDelta = (AttributeChangeModelDelta) delta;
+						if (attrDelta.getModelElement() instanceof CommunicationLink_c
+								&& attrDelta.getAttributeName().equals("Numb")) {
+							triggersWrite = true;
+						}
+						if (attrDelta.getModelElement() instanceof SynchronousMessage_c
+								&& attrDelta.getAttributeName().equals("Informalname")) {
+							triggersWrite = true;
+						}
+						if (attrDelta.getModelElement() instanceof AsynchronousMessage_c
+								&& attrDelta.getAttributeName().equals("Informalname")) {
+							triggersWrite = true;
+						}
+						if (attrDelta.getModelElement() instanceof ReturnMessage_c
+								&& attrDelta.getAttributeName().equals("Name")) {
+							triggersWrite = true;
+						}
+						if (attrDelta.getModelElement() instanceof Association_c && attrDelta.getAttributeName().equals("Numb")) {
+							triggersWrite = true;
+						}
+						if (triggersWrite) {
+							if(modelElement == null) {
+								modelElement = (NonRootModelElement) delta.getModelElement();
+							}
 							PersistableModelComponent pmc = modelElement.getPersistableComponent();
 							if (pmc != null) {
 								collectedComponents.add(pmc);
