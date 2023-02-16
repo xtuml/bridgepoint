78e0a86b8c4e5c158a471656908c72f94d69ca9a
Levi Starrett <levi@roxsoftware.com>
2023-02-16 15:37:22 -0500

    Update load and persist action

--- src/org.xtuml.bp.utilities/src/org/xtuml/bp/utilities/load/LoadAndPersistAction.java
+++ src/org.xtuml.bp.utilities/src/org/xtuml/bp/utilities/load/LoadAndPersistAction.java
@@ -1,19 +1,9 @@
-//====================================================================
-//
-// File:      $RCSfile: LoadAndPersistAction.java,v $
-// Version:   $Revision: 1.6 $
-// Modified:  $Date: 2013/01/10 23:21:54 $
-//
-// (c) Copyright 2005-2014 by Mentor Graphics Corp.  All rights reserved.
-//
-//====================================================================
-//
 package org.xtuml.bp.utilities.load;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -23,7 +13,6 @@ import org.eclipse.jface.dialogs.ProgressMonitorDialog;
 import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.ui.IActionDelegate;
 import org.eclipse.ui.PlatformUI;
 import org.xtuml.bp.core.CorePlugin;
@@ -32,12 +21,10 @@ import org.xtuml.bp.core.common.PersistableModelComponent;
 import org.xtuml.bp.core.common.PersistenceManager;
 import org.xtuml.bp.core.util.UIUtil;
 import org.xtuml.bp.ui.canvas.CanvasPlugin;
-import org.xtuml.bp.ui.canvas.references.ReferencePathManagement;
 
 public class LoadAndPersistAction implements IActionDelegate {
 
 	private List<NonRootModelElement> fElements = new ArrayList<>();
-	private List<NonRootModelElement> loadedGraphicalContainers = new ArrayList<>();
 
 	@Override
 	public void run(IAction action) {
@@ -46,55 +33,61 @@ public class LoadAndPersistAction implements IActionDelegate {
 		try {
 			dialog.run(false, true, new IRunnableWithProgress() {
 				public void run(IProgressMonitor monitor) {
-					monitor.beginTask("Load and persist of selected elements...", 100);
-					while(PlatformUI.getWorkbench().getDisplay().readAndDispatch());
-					loadedGraphicalContainers.clear();
+					final List<PersistableModelComponent> pmcsToProcess = fElements.stream()
+							.map(NonRootModelElement::getPersistableComponent)
+							.flatMap(pmc -> PersistenceManager.getDeepChildrenOf(pmc).stream()).distinct()
+							.collect(Collectors.toList());
+
+					// setup
+					monitor.beginTask("Load and persist of selected elements...", pmcsToProcess.size() * 3);
+					while (PlatformUI.getWorkbench().getDisplay().readAndDispatch())
+						; // wait for other tasks to finish
 					boolean succeeded = true;
 					long start = System.currentTimeMillis();
-					Iterator<NonRootModelElement> iter = fElements.iterator();
-					while (iter.hasNext()) {
-						if(monitor.isCanceled()) {
-							break;
-						}
-						monitor.subTask("Loading element...");
-						while(PlatformUI.getWorkbench().getDisplay().readAndDispatch());
-						NonRootModelElement nrme = iter.next();
-						PersistableModelComponent pmc = nrme.getPersistableComponent();
-						PersistenceManager.getDefaultInstance().loadProject(nrme, false, true);
-						monitor.worked(10 / fElements.size());
-						while(PlatformUI.getWorkbench().getDisplay().readAndDispatch());
-						collectLoadedRootElements(pmc);
-						try {
-							PlatformUI.getWorkbench().getDisplay().syncExec(() -> {
-								writeTextualGraphics(monitor);
-							});
-							if(monitor.isCanceled()) {
-								break;
+
+					// load collected PMCs
+					try {
+						PersistenceManager.getDefaultInstance().loadComponents(pmcsToProcess, new NullProgressMonitor(),
+								false, true);
+						monitor.worked(pmcsToProcess.size());
+					} catch (CoreException e) {
+						CorePlugin.logError("Failed to load collected components", e);
+						succeeded = false;
+						monitor.setCanceled(true);
+					}
+
+					// write textual graphics
+					CanvasPlugin.getDefault().getPersistenceExtensionRegistry().getExtensions().forEach(pe -> {
+						pmcsToProcess.stream().map(PersistableModelComponent::getRootModelElement)
+								.forEach(modelElement -> {
+									if (!monitor.isCanceled()) {
+										pe.getWriter().write(modelElement, true);
+										monitor.worked(1);
+										while (PlatformUI.getWorkbench().getDisplay().readAndDispatch())
+											;
+									}
+								});
+					});
+
+					// write PMCs
+					for (PersistableModelComponent pmc : pmcsToProcess) {
+						if (succeeded && !monitor.isCanceled()) {
+							try {
+								pmc.persist();
+								monitor.worked(1);
+							} catch (CoreException e) {
+								CorePlugin.logError("Failed to perist component", e);
+								succeeded = false;
+								monitor.setCanceled(true);
 							}
-						} catch (Exception e) {
-							// catch any exception, we do not want to persist
-							// in the case where graphical instances are not
-							// configured to be written, as we will lose graphics
-							String message = "Unable to load and persist textual graphics, operation aborted.";
-							CorePlugin.logError(message, e);
-							monitor.setCanceled(true);
-						}
-						try {
-							monitor.subTask("Persisting element...");
-							pmc.persistSelfAndChildren();
-							monitor.worked(10 / fElements.size());
-							while(PlatformUI.getWorkbench().getDisplay().readAndDispatch());
-							LoadingUtilities.loadByTreeExpansion(nrme);
-						} catch (CoreException ce) {
-							succeeded = false;
-							CorePlugin.logError("Unable to persist the selection.", ce);
-							monitor.setCanceled(true);
 						}
 					}
+
+					// finish
 					if (succeeded) {
-						long end = System.currentTimeMillis();
-						double duration = ((double) end - start) / 1000;
-						String msg = "Load and Persist duration: " + duration + " seconds";
+						final long end = System.currentTimeMillis();
+						final double duration = ((double) end - start) / 1000;
+						final String msg = "Load and Persist duration: " + duration + " seconds";
 						UIUtil.openInformation(PlatformUI.getWorkbench().getDisplay().getActiveShell(),
 								"Load and Persist", msg);
 					}
@@ -106,42 +99,12 @@ public class LoadAndPersistAction implements IActionDelegate {
 		}
 	}
 
-	private void collectLoadedRootElements(PersistableModelComponent pmc) {
-		if (ReferencePathManagement.elementHasDiagramRepresentation(pmc.getRootModelElement())) {
-			loadedGraphicalContainers.add(pmc.getRootModelElement());
-		}
-		pmc.getChildren().forEach(child -> {
-			collectLoadedRootElements((PersistableModelComponent) child);
-		});
-	}
-
-	private void writeTextualGraphics(IProgressMonitor monitor) {
-		monitor.subTask("Writing textual graphics...");
-		// persist all textual graphics files
-		CanvasPlugin.getDefault().getPersistenceExtensionRegistry().getExtensions().forEach(pe -> {
-			loadedGraphicalContainers.forEach(modelElement -> {
-				if(!monitor.isCanceled()) {
-					pe.getWriter().write(modelElement, true);
-					monitor.worked((int) (80 / loadedGraphicalContainers.size()));
-					while(PlatformUI.getWorkbench().getDisplay().readAndDispatch());
-				}
-			});
-		});
-	}
-
 	@Override
 	public void selectionChanged(IAction action, ISelection selection) {
-		fElements.clear();
-		if (selection instanceof StructuredSelection) {
-			IStructuredSelection ss = ((IStructuredSelection) selection);
-			// cache selection of model elements
-			Iterator<?> iter = ss.iterator();
-			while (iter.hasNext()) {
-				Object obj = iter.next();
-				if (obj instanceof NonRootModelElement) {
-					fElements.add((NonRootModelElement) obj);
-				}
-			}
+		if (selection instanceof IStructuredSelection) {
+			fElements = ((List<?>) ((IStructuredSelection) selection).toList()).stream()
+					.filter(NonRootModelElement.class::isInstance).map(NonRootModelElement.class::cast)
+					.collect(Collectors.toList());
 		}
 	}
 
