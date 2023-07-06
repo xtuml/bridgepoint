0826bb96d4214729288f6af3302f904d74f2dad2
John R. Wolfe <johnwolfe@mailetechnical.com>
2023-07-06 11:06:48 -0700

    Revert "Configured for Linux"
    This reverts commit e0caedcfb9cc22cedf1be01210cef43df180bb63.

--- utilities/build/build_configuration.sh
+++ utilities/build/build_configuration.sh
@@ -8,10 +8,10 @@ fi
 
 if [ -z $bp_install_dir ]
 then
-  # for mac use this install dir
-  # export bp_install_dir=~/xtuml/BridgePoint.app/Contents/Eclipse/
+  # for mac us this install dir
+  export bp_install_dir=~/xtuml/BridgePoint.app/Contents/Eclipse/
   # for linux uncomment this line and comment the mac version
-  export bp_install_dir=~/xtuml/BridgePoint
+  #export bp_install_dir=~/xtuml/BridgePoint
 fi
 
 if [ -z $WORKSPACE ]
