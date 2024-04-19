e1a15dc698625f28ed9a0cc3866aa9af373eebd6
Levi Starrett <levi@roxsoftware.com>
2024-04-19 11:25:10 -0400

    job #12806: add support for executing instance-based operations in verifier

--- src/org.xtuml.bp.core/sql/ooaofooa_hierarchy.pei.sql
+++ src/org.xtuml.bp.core/sql/ooaofooa_hierarchy.pei.sql
@@ -28,6 +28,7 @@ INSERT INTO T_TNS VALUES (3,  'Function.gif',                   'S_SYNC',  '', '
 INSERT INTO T_TNS VALUES (4,  'FunctionParameter.gif',          'S_SPARM', '', 'Name', '',                 'Function Parameter',        54, 'succeeds');
 INSERT INTO T_TNS VALUES (7,  'InstanceBasedOperation.gif',                  'O_TFR',   '', 'Name', '',                 'Operation',                 125, 'succeeds');
 INSERT INTO T_TNS VALUES (8,  'OperationParameter.gif',         'O_TPARM', '', 'Name', '',                 'Operation Parameter',       124, 'succeeds');
+INSERT INTO T_TNS VALUES (9,  'InstanceBasedOperation.gif', 'I_IBO', '', 'Name', '->O_TFR[R2979]', 'Instance Bound Operations', -1, '');
 INSERT INTO T_TNS VALUES (10, 'InstanceStateChart.gif',         'SM_ISM',  'Instance State Chart', '', '', 'Instance State Chart', 		-1, '');
 INSERT INTO T_TNS VALUES (15, 'ClassStateChart.gif',            'SM_ASM',  'Class State Chart', '', '',    'Class State Chart', 			-1, '');
 INSERT INTO T_TNS VALUES (11, 'State.gif',                      'SM_STATE','', 'Name', '',                 'State', 						-1, '');
